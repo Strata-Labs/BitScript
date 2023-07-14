bfdf654d0b658b727909230bb83039e62062faf4
Pato Gomez <pato2k5@gmail.com>
2023-07-14 10:42:18 -0600

    View Scripts

--- src/comp/ContentContainer.tsx
+++ src/comp/ContentContainer.tsx
@@ -6,52 +6,52 @@ const ContentContainer: React.FC = () => {
     <div className="h-screen overflow-auto w-screen">
       <div className="w-screen flex flex-col">
         <p className="text-[14px] font-extralight absolute top-0 left-0 mt-24 ml-9 md:ml-[270px] text-[#6C5E70]">Scripts</p>
-        <div className='flex'>
-        <p className="mt-36 ml-9 md:ml-[270px] text-[20px] text-[#0C071D] font-semibold">Select A Bitcoin Script</p>
-        <p className="mt-36 ml-1 text-[20px] text-[#0C071D] font-semibold md:block hidden">Format To Explore</p>
+        <div className='flex flex-wrap'>
+          <p className="mt-36 ml-9 md:ml-[270px] text-[20px] text-[#0C071D] font-semibold">Select A Bitcoin Script</p>
+          <p className="mt-36 ml-1 text-[20px] text-[#0C071D] font-semibold md:block hidden">Format To Explore</p>
         </div>
         <p className="ml-9 md:ml-[270px] text-[20px] text-[#0C071D] font-semibold md:hidden">Format To Explore</p>
-        <div className='flex flex-col md:flex-row md:ml-[230px]'>
-        <div className='mt-5'>
-          <ScriptContainer
-            scriptName="Script Name"
-            scriptDescription="P2PK"
-            summary="The most basic script for a direct transfer. Rarely used, but a good starting point"
-            introduction="BIP133"
-            inUse="Yes"
-            numberOfOPs="14"
-          />
-        </div>
-        <div className='mt-3'>
-          <ScriptContainer
-            scriptName="Script Name"
-            scriptDescription="P2PK"
-            summary="The most basic script for a direct transfer."
-            introduction="BIP133"
-            inUse="Yes"
-            numberOfOPs="14"
-          />
-        </div>
-        <div className='mt-3'>
-          <ScriptContainer
-            scriptName="Script Name"
-            scriptDescription="P2PK"
-            summary="The most basic script for a direct transfer."
-            introduction="BIP133"
-            inUse="Yes"
-            numberOfOPs="14"
-          />
-        </div>
-        <div className='mt-3'>
-          <ScriptContainer
-            scriptName="Script Name"
-            scriptDescription="P2PK"
-            summary="The most basic script for a direct transfer."
-            introduction="BIP133"
-            inUse="Yes"
-            numberOfOPs="14"
-          />
-        </div>
+        <div className='flex flex-col md:flex-row md:flex-wrap md:ml-[230px]'>
+          <div className='mt-5 md:w-11/12 lg:w-1/2 xl:w-2/6 2xl:w-1/4'>
+            <ScriptContainer
+              scriptName="Script Name"
+              scriptDescription="P2PK"
+              summary="The most basic script for a direct transfer. Rarely used, but a good starting point"
+              introduction="BIP133"
+              inUse="Yes"
+              numberOfOPs="14"
+            />
+          </div>
+          <div className='mt-5 md:w-11/12 lg:w-1/2 xl:w-2/6 2xl:w-1/4'>
+            <ScriptContainer
+              scriptName="Script Name"
+              scriptDescription="P2PK"
+              summary="The most basic script for a direct transfer."
+              introduction="BIP133"
+              inUse="Yes"
+              numberOfOPs="14"
+            />
+          </div>
+          <div className='mt-5 md:w-11/12 lg:w-1/2 xl:w-2/6 2xl:w-1/4'>
+            <ScriptContainer
+              scriptName="Script Name"
+              scriptDescription="P2PK"
+              summary="The most basic script for a direct transfer."
+              introduction="BIP133"
+              inUse="Yes"
+              numberOfOPs="14"
+            />
+          </div>
+          <div className='mt-5 md:w-11/12 lg:w-1/2 xl:w-2/6 2xl:w-1/4'>
+            <ScriptContainer
+              scriptName="Script Name"
+              scriptDescription="P2PK"
+              summary="The most basic script for a direct transfer."
+              introduction="BIP133"
+              inUse="Yes"
+              numberOfOPs="14"
+            />
+          </div>
         </div>
       </div>
     </div>
