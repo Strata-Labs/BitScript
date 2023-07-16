3be6f7505ca8de2e3e41ed05428114cad43a2732
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-07-16 15:31:09 -0400

    remove testing start

--- src/comp/MultiSections.tsx
+++ src/comp/MultiSections.tsx
@@ -286,12 +286,12 @@ const MultiSections = () => {
         >
           Back
         </button>
-        <button
+        {/* <button
           type="button"
           className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
         >
           Start
-        </button>
+        </button> */}
         <button
           onClick={() => handleNext()}
           //onClick={() => addOpAdd()}
