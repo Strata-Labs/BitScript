9b7be394871d98bfcbdb686f0b87c7854a37404c
Pato Gomez <pato2k5@gmail.com>
2023-07-17 07:49:43 -0600

    OpCodes Views

--- src/comp/ScriptsPage.tsx
+++ src/comp/ScriptsPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Layout from './Layout';
 import NavigationMobile from './NavigationMobile';
-import Navigation from './Navigation';
 import ScriptViews from './ScriptViews';
 import ScriptContent from './ScriptContent';
 
