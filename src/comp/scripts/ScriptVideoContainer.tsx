85507d5a6d36b80ebe3662dca1c06ff28dbaee02
Pato Gomez <pato2k5@gmail.com>
2023-07-24 11:27:23 -0600

    Script Views

--- src/comp/scripts/ScriptVideoContainer.tsx
+++ src/comp/scripts/ScriptVideoContainer.tsx
@@ -2,47 +2,85 @@ import React from 'react';
 
 const BottomVideoContainer: React.FC = () => {
   return (
-<div className="flex flex-col items-center justify-center">
-  <div className="m-4 bg-white h-[600px] w-[400px] rounded-xl flex items-center flex-col md:w-[1156px] md:ml-[256px] md:flex-row md:items-start">
+<div className="flex-col md:items-start items-center justify-center">
+  <div className="mt-4 lg:pb-4 bg-white md:h-[420px] h-[614px] rounded-xl flex items-center flex-col md:min-w-[1200px] md:items-start md:ml-[267px] md:justify-between md:mr-8 ml-12 mr-12 mb-5">
     {/* Black Rectangle Container */}
-    <div className="mt-4 bg-[#26292C] h-[300px] w-[350px] rounded-xl flex flex-col md:h-[303px] md:w-[494px] md:ml-7 md:mt-6">
-      <div className='flex bg-[#1C1E20] w-[350px] h-[35px] rounded-t-lg items-start'>
-        {/* 3 buttons */}
-        <button className='h-[9px] w-[9px] rounded-full bg-[#F45952] mt-3 ml-3'></button>
-        <button className='h-[9px] w-[9px] rounded-full bg-[#DFB94E] mt-3 ml-3'></button>
-        <button className='h-[9px] w-[9px] rounded-full bg-[#5AB748] mt-3 ml-3'></button>
+    <div className='flex flex-col md:justify-between mt-4'>
+      <div className='flex flex-col md:flex-row md:justify-between mt-4'>
+        <div className="bg-[#26292C] h-[200px] w-[350px] rounded-xl flex flex-col md:h-[303px] md:w-[494px] md:ml-7 md:mt-0 ">
+          <div className='flex bg-[#1C1E20] w-[350px] md:w-[494px] h-[35px] rounded-t-lg items-start'>
+            {/* 3 buttons */}
+            <button className='h-[9px] w-[9px] rounded-full bg-[#F45952] mt-3 ml-3'></button>
+            <button className='h-[9px] w-[9px] rounded-full bg-[#DFB94E] mt-3 ml-3'></button>
+            <button className='h-[9px] w-[9px] rounded-full bg-[#5AB748] mt-3 ml-3'></button>
+          </div>
+          {/* Information */}
+          <div className='flex flex-col mt-1 ml-3'>
+            <p className='text-[#787878] text-[11px] md:text-[16px]'># UnlockScript/ScriptSig</p>
+            <p className='text-[11px] md:text-[16px]'>[signature]</p>
+            <p className='text-[11px] md:text-[16px]'>[public-key]</p>
+            <p className='mt-2 text-[#787878] text-[11px] md:text-[16px]'># LockScript/ScriptPubKey</p>
+            <p className='text-[11px] md:text-[16px]'>&lt;dup&gt;</p>
+            <p className='text-[11px] md:text-[16px]'>&lt;hash160&gt;</p>
+            <p className='text-[11px] md:text-[16px]'>[hash160[publick-key]]</p>
+            <p className='ml-1 text-[11px] md:text-[16px]'>&lt;equalverify&gt;</p>
+            <p className='text-[11px] md:text-[16px]'>&lt;checksig&gt;</p>
+          </div>
+        </div>
+        {/* Video Section */}
+        <div className='flex flex-col md:mr-5 md:ml-10'>
+          {/* Titles */}
+          <div className='flex items-center justify-center mt-5 md:mt-0 md:items-start md:justify-start'>
+            <p className='text-black font-semibold md:text-[16px]'>Press Play Button Below</p>
+            <p className='ml-5 text-[12px] text-[#CAC6DD] hidden md:block'>OR</p>
+            <p className='text-black ml-5 font-semibold hidden md:block'>Tap The Spacebar</p>
+          </div>
+          {/* Video */}
+          <div className='flex mt-5 bg-[#F9F9F9] w-[350px] h-[200px] md:min-w-[594px] md:h-[260px] rounded-lg'>
+          </div>
+        </div>
       </div>
-      {/* Information */}
-      <div className='flex flex-col mt-3 ml-3'>
-        <p className='text-[#787878]'># UnlockScript/ScriptSig</p>
-        <p>[signature]</p>
-        <p>[public-key]</p>
-        <p className='mt-4 text-[#787878]'># LockScript/ScriptPubKey</p>
-        <p>&lt;dup&gt;</p>
-        <p>&lt;hash160&gt;</p>
-        <p>[hash160[publick-key]]</p>
-        <p className='ml-1'>&lt;equalverify&gt;</p>
-        <p>&lt;checksig&gt;</p>
-      </div>
-    </div>
-    {/* Video Section */}
-    <div className='flex md:flex-col ml-10'>
-      {/* Titles */}
-      <div className='flex items-center mt-3 md:mt-0'>
-        <p className='text-black font-semibold md:text-[16px]'>Press Play Button Below</p>
-        <p className='ml-2 text-[12px] text-[#CAC6DD]'>OR</p>
-        <p className='text-black ml-2 font-semibold'>Tap The Spacebar</p>
-      </div>
-      {/* Loading Video Bar */}
-      <div className='flex bg-[#F9F9F9] w-[350px] h-[200px] rounded-lg mt-1'></div>
-      {/* Media Bar */}
-      <div className='flex bg-[#F9F9F9] w-[350px] h-[30px] rounded-lg mt-1 items-center'>
+      {/* Media Buttons Bar Desktop */}
+      <div className='md:flex bg-[#F9F9F9] w-[1130px] h-[50px] rounded-xl mt-4 ml-7 items-center justify-start hidden'>
         {/* Fast Rewind */}
-        <button>
+        <button className='ml-5'>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-2'>
             <path d="M20.226 5.52197C19.291 5.06997 18.207 5.18901 17.392 5.83301L12.75 9.49805V7.96411C12.75 6.91411 12.166 5.97895 11.226 5.52295C10.291 5.07095 9.20697 5.18998 8.39197 5.83398L3.75 9.49902V6C3.75 5.586 3.414 5.25 3 5.25C2.586 5.25 2.25 5.586 2.25 6V18C2.25 18.414 2.586 18.75 3 18.75C3.414 18.75 3.75 18.414 3.75 18V14.501L8.39197 18.168C8.87897 18.552 9.462 18.75 10.052 18.75C10.449 18.75 10.85 18.66 11.226 18.479C12.166 18.023 12.75 17.0881 12.75 16.0381V14.5029L17.392 18.1699C17.879 18.5539 18.462 18.752 19.052 18.752C19.449 18.752 19.85 18.662 20.226 18.481C21.166 18.025 21.75 17.09 21.75 16.04V7.96704C21.75 6.91304 21.166 5.97797 20.226 5.52197ZM11.25 16.0371C11.25 16.5151 10.996 16.9219 10.571 17.1279C10.153 17.3309 9.68704 17.278 9.32104 16.991L4.20996 12.9541C3.91796 12.7231 3.75 12.375 3.75 12C3.75 11.625 3.91796 11.2769 4.20996 11.0459L9.32104 7.01001C9.68704 6.72201 10.153 6.67107 10.571 6.87207C10.996 7.07807 11.25 7.48589 11.25 7.96289V16.0371ZM20.25 16.0371C20.25 16.5151 19.996 16.9219 19.571 17.1279C19.153 17.3309 18.687 17.278 18.321 16.991L13.21 12.9541C12.918 12.7231 12.75 12.375 12.75 12C12.75 11.625 12.918 11.2769 13.21 11.0459L18.321 7.01001C18.687 6.72201 19.154 6.67107 19.571 6.87207C19.996 7.07807 20.25 7.48589 20.25 7.96289V16.0371Z" fill="#D2D2D2"/>
           </svg>
         </button>
+        {/* Hard Rewind */}
+        <button className='ml-5'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.8571 3.58691C16.7001 3.02691 15.353 3.17292 14.343 3.97192L7.52795 9.354C7.20495 9.609 6.955 9.92389 6.75 10.2649V4C6.75 3.586 6.414 3.25 6 3.25C5.586 3.25 5.25 3.586 5.25 4V20C5.25 20.414 5.586 20.75 6 20.75C6.414 20.75 6.75 20.414 6.75 20V13.7329C6.956 14.0739 7.20595 14.389 7.52795 14.644L14.343 20.0271C14.947 20.5041 15.67 20.749 16.402 20.749C16.895 20.749 17.3911 20.6381 17.8571 20.4131C19.0241 19.8471 19.75 18.6861 19.75 17.3831V6.61792C19.75 5.31392 19.0241 4.15291 17.8571 3.58691ZM18.25 17.3821C18.25 18.1161 17.858 18.745 17.203 19.062C16.558 19.374 15.8379 19.2951 15.2729 18.8501L8.45801 13.467C8.00801 13.112 7.75 12.576 7.75 11.999C7.75 11.421 8.00801 10.886 8.45801 10.531L15.2729 5.14893C15.8369 4.70393 16.558 4.62601 17.203 4.93701C17.858 5.25401 18.25 5.88294 18.25 6.61694V17.3821Z" fill="#D2D2D2"/>
+          </svg>
+        </button>
+        {/* Play */}
+        <button className='ml-5'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18.66 14.3869L8.58398 20.5529C6.57898 21.7799 4 20.3408 4 17.9948V6.00575C4 3.65975 6.57898 2.22089 8.58398 3.44789L18.66 9.6139C20.445 10.7069 20.445 13.2949 18.66 14.3869Z" fill="#25314C"/>
+          </svg>
+        </button>
+        {/* Fast Forward */}
+        <button className='ml-5'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 3.25C17.586 3.25 17.25 3.586 17.25 4V10.2661C17.044 9.92511 16.794 9.60998 16.472 9.35498L9.65698 3.9729C8.64598 3.1739 7.30094 3.02889 6.14294 3.58789C4.97594 4.15389 4.25 5.31492 4.25 6.61792V17.3831C4.25 18.6861 4.97594 19.8471 6.14294 20.4131C6.60894 20.6391 7.10502 20.749 7.59802 20.749C8.32902 20.749 9.05298 20.5041 9.65698 20.0271L16.472 14.644C16.795 14.389 17.045 14.0739 17.25 13.7329V20C17.25 20.414 17.586 20.75 18 20.75C18.414 20.75 18.75 20.414 18.75 20V4C18.75 3.586 18.414 3.25 18 3.25ZM15.542 13.467L8.72705 18.8501C8.16405 19.2951 7.442 19.373 6.797 19.062C6.142 18.745 5.75 18.1161 5.75 17.3821V6.61694C5.75 5.88294 6.142 5.25401 6.797 4.93701C7.058 4.81101 7.32995 4.74902 7.60095 4.74902C7.99895 4.74902 8.39105 4.88393 8.72705 5.14893L15.542 10.531C15.992 10.886 16.25 11.422 16.25 11.999C16.25 12.577 15.992 13.112 15.542 13.467Z" fill="#D2D2D2"/>
+          </svg>
+        </button>
+        {/* Hard Forward */}
+        <button className='ml-5'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 5.25C20.586 5.25 20.25 5.586 20.25 6V9.49805L15.608 5.83203C14.793 5.18903 13.709 5.07097 12.774 5.52197C11.834 5.97797 11.25 6.91289 11.25 7.96289V9.49707L6.60803 5.83105C5.79303 5.18805 4.70905 5.07 3.77405 5.521C2.83405 5.977 2.25 6.91191 2.25 7.96191V16.0349C2.25 17.0849 2.83405 18.0201 3.77405 18.4761C4.15005 18.6581 4.55 18.7471 4.948 18.7471C5.538 18.7471 6.12103 18.55 6.60803 18.165L11.25 14.499V16.0339C11.25 17.0839 11.834 18.0191 12.774 18.4751C13.15 18.6571 13.55 18.7461 13.948 18.7461C14.538 18.7461 15.121 18.5491 15.608 18.1641L20.25 14.498V18C20.25 18.414 20.586 18.75 21 18.75C21.414 18.75 21.75 18.414 21.75 18V6C21.75 5.586 21.414 5.25 21 5.25ZM10.79 12.9541L5.67896 16.991C5.31396 17.28 4.84796 17.3309 4.42896 17.1289C4.00396 16.9229 3.75 16.5151 3.75 16.0381V7.96509C3.75 7.48709 4.00396 7.08002 4.42896 6.87402C4.59796 6.79202 4.77495 6.75195 4.94995 6.75195C5.20795 6.75195 5.46196 6.83999 5.67896 7.01099L10.79 11.0471C11.082 11.2781 11.25 11.626 11.25 12.001C11.25 12.376 11.082 12.7231 10.79 12.9541ZM19.79 12.9541L14.679 16.991C14.314 17.28 13.848 17.3309 13.429 17.1289C13.004 16.9229 12.75 16.5151 12.75 16.0381V7.96509C12.75 7.48709 13.004 7.08002 13.429 6.87402C13.598 6.79202 13.775 6.75195 13.95 6.75195C14.208 6.75195 14.462 6.83999 14.679 7.01099L19.79 11.0471C20.082 11.2781 20.25 11.626 20.25 12.001C20.25 12.376 20.082 12.7231 19.79 12.9541Z" fill="#D2D2D2"/>
+          </svg>
+        </button>
+      </div>
+      {/* Media Buttons Mobile */}
+      <div className='flex justify-between mt-5 md:hidden'>
+          {/* Fast Rewind */}
+          <button>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-2'>
+              <path d="M20.226 5.52197C19.291 5.06997 18.207 5.18901 17.392 5.83301L12.75 9.49805V7.96411C12.75 6.91411 12.166 5.97895 11.226 5.52295C10.291 5.07095 9.20697 5.18998 8.39197 5.83398L3.75 9.49902V6C3.75 5.586 3.414 5.25 3 5.25C2.586 5.25 2.25 5.586 2.25 6V18C2.25 18.414 2.586 18.75 3 18.75C3.414 18.75 3.75 18.414 3.75 18V14.501L8.39197 18.168C8.87897 18.552 9.462 18.75 10.052 18.75C10.449 18.75 10.85 18.66 11.226 18.479C12.166 18.023 12.75 17.0881 12.75 16.0381V14.5029L17.392 18.1699C17.879 18.5539 18.462 18.752 19.052 18.752C19.449 18.752 19.85 18.662 20.226 18.481C21.166 18.025 21.75 17.09 21.75 16.04V7.96704C21.75 6.91304 21.166 5.97797 20.226 5.52197ZM11.25 16.0371C11.25 16.5151 10.996 16.9219 10.571 17.1279C10.153 17.3309 9.68704 17.278 9.32104 16.991L4.20996 12.9541C3.91796 12.7231 3.75 12.375 3.75 12C3.75 11.625 3.91796 11.2769 4.20996 11.0459L9.32104 7.01001C9.68704 6.72201 10.153 6.67107 10.571 6.87207C10.996 7.07807 11.25 7.48589 11.25 7.96289V16.0371ZM20.25 16.0371C20.25 16.5151 19.996 16.9219 19.571 17.1279C19.153 17.3309 18.687 17.278 18.321 16.991L13.21 12.9541C12.918 12.7231 12.75 12.375 12.75 12C12.75 11.625 12.918 11.2769 13.21 11.0459L18.321 7.01001C18.687 6.72201 19.154 6.67107 19.571 6.87207C19.996 7.07807 20.25 7.48589 20.25 7.96289V16.0371Z" fill="#D2D2D2"/>
+            </svg>
+          </button>
         {/* Hard Rewind */}
         <button>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -50,8 +88,8 @@ const BottomVideoContainer: React.FC = () => {
           </svg>
         </button>
         {/* Play */}
-        <button>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <button className='h-[44px] w-[44px] bg-[#F1F1F1] flex items-center justify-center rounded-full'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='ml-1'>
             <path d="M18.66 14.3869L8.58398 20.5529C6.57898 21.7799 4 20.3408 4 17.9948V6.00575C4 3.65975 6.57898 2.22089 8.58398 3.44789L18.66 9.6139C20.445 10.7069 20.445 13.2949 18.66 14.3869Z" fill="#25314C"/>
           </svg>
         </button>
@@ -68,6 +106,7 @@ const BottomVideoContainer: React.FC = () => {
           </svg>
         </button>
       </div>
+      {/* Media Playing Bar Mobile*/}
     </div>
   </div>
 </div>
--- src/comp/scripts/p2pkh.tsx
+++ src/comp/scripts/p2pkh.tsx
@@ -4,9 +4,10 @@ import BottomVideoContainer from './ScriptVideoContainer';
 
 const ScriptContent: React.FC = () => {
   return (
-<div className="h-screen w-screen">
-    <div className='w-screen flex justify-between md:mt-5 mt-[100px]'>
-        <div className='flex justify-start md:ml-[260px]'>
+<div className="h-screen overflow-auto w-screen">
+    <div className='w-screen flex flex-col md:flex-row justify-between md:mt-5 mt-[100px]'>
+        <div className='flex flex-col md:flex-row justify-start md:ml-[260px] ml-10'>
+            {/* Left pointing icon link */}
             <Link 
             href="/scripts" 
             className=""
@@ -25,25 +26,31 @@ const ScriptContent: React.FC = () => {
                 </g>
                 </svg>
             </Link>
-            <p className='text-[#0C071D] font-semibold ml-2 md:ml-4 text-[20px] mt-[17px] md:mt-0 md:text-[28px]'>P2PKH</p>
-            <p className='text-[#687588] ml-[8px] font-extralight text-[12px] mt-[22px] md:text-[18px] md:mt-2'>(pay to public key hash)</p>
+            {/* Title and description */}
+            <div className='flex'>
+                <p className='text-[#0C071D] font-semibold ml-2 md:ml-4 text-[20px] mt-[17px] md:mt-0 md:text-[28px]'>P2PKH</p>
+                <p className='text-[#687588] ml-[8px] font-extralight text-[12px] mt-[22px] md:text-[18px] md:mt-2'>(pay to public key hash)</p>
+            </div>
         </div>
-        <div className='flex justify-end mr-6'>
+        <div className='flex md:justify-end md:mr-6 ml-12'>
             <Link href={''} className='text-[#F79327] underline text-sm md:text-md md:mt-0 mt-5'>Latest on-chain-example</Link>
         </div>
     </div>
-    <div className='flex flex-col items-start ml-4 mr-4 md:ml-[265px] md:mr-[200px] mt-7'>
-        <p className='text-[#6C5E70] text-[16px] font-extralight'>A Pay-to-Public-Key-Hash (P2PKH) script is a common type of Bitcoin transaction script that allows bitcoins to be sent to a specific Bitcoin address. The script locks the bitcoins to the hash of a public key, requiring a signature from the corresponding private key to spend them. When the bitcoins are spent, the spender provides a scriptSig that includes the public key and a valid signature.</p>
-        <p className='text-black font-semibold mt-[60px] text-[18px]'>OP_Code(s) Review</p>
-        <p className='text-[#6C5E70] text-[16px] font-extralight mt-[20px]'>P2PKH requires three (3) pieces of data & four (4) op_codes. The three (3) editable data items required are seen below. </p>
+    {/* Paragraph */}
+    <div className='flex flex-col items-start ml-12 mr-12 md:ml-[265px] md:mr-[200px] mt-7'>
+        <p className='text-[#6C5E70] text-[14px] md:text-[16px] font-extralight'>A Pay-to-Public-Key-Hash (P2PKH) script is a common type of Bitcoin transaction script that allows bitcoins to be sent to a specific Bitcoin address. The script locks the bitcoins to the hash of a public key, requiring a signature from the corresponding private key to spend them. When the bitcoins are spent, the spender provides a scriptSig that includes the public key and a valid signature.</p>
+        <p className='text-black font-semibold mt-[30px] md:mt-[60px] text-[18px]'>OP_Code(s) Review</p>
+        <p className='text-[#6C5E70] text-[14px] md:text-[16px] font-extralight mt-[20px]'>P2PKH requires three (3) pieces of data & four (4) op_codes. The three (3) editable data items required are seen below. </p>
     </div>
-    <div className='flex flex-col justify-center items-center md:justify-start md:flex-row md:flex-wrap md:ml-[265px] mt-6 mr-5 ml-4'>
+    <div className='flex flex-col justify-center items-center md:justify-start md:flex-row md:flex-wrap md:ml-[265px] mt-6'>
         <div className='flex'>
-            <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13.999 19.249C13.851 19.249 13.7011 19.205 13.5701 19.114C13.2301 18.877 13.148 18.4091 13.385 18.0701C13.983 17.2151 14.249 16.113 14.249 14.5V8.5C14.249 7.188 13.8521 5.94195 13.1011 4.89795C12.8591 4.56195 12.935 4.09307 13.271 3.85107C13.609 3.60907 14.0771 3.68597 14.3181 4.02197C15.2551 5.32297 15.75 6.872 15.75 8.5V14.5C15.75 16.44 15.399 17.8069 14.614 18.9299C14.469 19.1369 14.236 19.249 13.999 19.249ZM1.75 14.499V8.49902C1.75 5.05302 4.554 2.24902 8 2.24902C8.22 2.24902 8.43891 2.25694 8.65991 2.28394C9.06791 2.32594 9.4439 2.04113 9.4939 1.63013C9.5439 1.21813 9.25109 0.844922 8.84009 0.794922C8.56109 0.760922 8.28 0.75 8 0.75C3.727 0.75 0.25 4.227 0.25 8.5V14.5C0.25 14.914 0.586 15.25 1 15.25C1.414 15.25 1.75 14.913 1.75 14.499ZM8.41699 20.123C10.66 18.624 11.75 16.5851 11.75 13.8901V8.49902C11.75 7.49902 11.3589 6.55814 10.6499 5.84814C9.9739 5.17414 9.04089 4.77302 8.08789 4.74902C7.68689 4.74002 7.33009 5.06698 7.31909 5.47998C7.30909 5.89398 7.63605 6.23802 8.05005 6.24902C8.62105 6.26302 9.18289 6.50394 9.58789 6.90894C10.0139 7.33494 10.248 7.89902 10.248 8.49902V13.8889C10.248 16.0749 9.40005 17.659 7.58105 18.875C7.23705 19.105 7.14402 19.571 7.37402 19.916C7.52002 20.132 7.75702 20.249 7.99902 20.249C8.14402 20.249 8.28899 20.208 8.41699 20.123ZM4.56396 15.926C5.32896 14.93 5.75 13.688 5.75 12.429V8.49902C5.75 8.26502 5.78104 8.04497 5.84204 7.86597C5.97304 7.47297 5.75997 7.04797 5.36597 6.91797C4.97297 6.78497 4.54897 7.00007 4.41797 7.39307C4.30697 7.72807 4.25 8.101 4.25 8.5V12.4299C4.25 13.3599 3.939 14.2779 3.375 15.0129C3.122 15.3419 3.18494 15.8119 3.51294 16.0649C3.64994 16.1689 3.80999 16.22 3.96899 16.22C4.19399 16.219 4.41697 16.118 4.56396 15.926ZM8.75 13.499V9.49902C8.75 9.08502 8.414 8.74902 8 8.74902C7.586 8.74902 7.25 9.08502 7.25 9.49902V13.499C7.25 13.913 7.586 14.249 8 14.249C8.414 14.249 8.75 13.913 8.75 13.499Z" fill="#0C071D"/>
-            </svg>
-            <p className='text-black ml-3 md:ml-5'>Signature</p>
-            <div className="bg-[#0C071D] bg-opacity-10 h-[31px] w-[228px] ml-7 md:ml-9 xl:ml-3 rounded-full -mt-1 items-center justify-center flex">
+            <div className='flex'>
+                <svg width="16" height="21" viewBox="0 0 16 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.999 19.249C13.851 19.249 13.7011 19.205 13.5701 19.114C13.2301 18.877 13.148 18.4091 13.385 18.0701C13.983 17.2151 14.249 16.113 14.249 14.5V8.5C14.249 7.188 13.8521 5.94195 13.1011 4.89795C12.8591 4.56195 12.935 4.09307 13.271 3.85107C13.609 3.60907 14.0771 3.68597 14.3181 4.02197C15.2551 5.32297 15.75 6.872 15.75 8.5V14.5C15.75 16.44 15.399 17.8069 14.614 18.9299C14.469 19.1369 14.236 19.249 13.999 19.249ZM1.75 14.499V8.49902C1.75 5.05302 4.554 2.24902 8 2.24902C8.22 2.24902 8.43891 2.25694 8.65991 2.28394C9.06791 2.32594 9.4439 2.04113 9.4939 1.63013C9.5439 1.21813 9.25109 0.844922 8.84009 0.794922C8.56109 0.760922 8.28 0.75 8 0.75C3.727 0.75 0.25 4.227 0.25 8.5V14.5C0.25 14.914 0.586 15.25 1 15.25C1.414 15.25 1.75 14.913 1.75 14.499ZM8.41699 20.123C10.66 18.624 11.75 16.5851 11.75 13.8901V8.49902C11.75 7.49902 11.3589 6.55814 10.6499 5.84814C9.9739 5.17414 9.04089 4.77302 8.08789 4.74902C7.68689 4.74002 7.33009 5.06698 7.31909 5.47998C7.30909 5.89398 7.63605 6.23802 8.05005 6.24902C8.62105 6.26302 9.18289 6.50394 9.58789 6.90894C10.0139 7.33494 10.248 7.89902 10.248 8.49902V13.8889C10.248 16.0749 9.40005 17.659 7.58105 18.875C7.23705 19.105 7.14402 19.571 7.37402 19.916C7.52002 20.132 7.75702 20.249 7.99902 20.249C8.14402 20.249 8.28899 20.208 8.41699 20.123ZM4.56396 15.926C5.32896 14.93 5.75 13.688 5.75 12.429V8.49902C5.75 8.26502 5.78104 8.04497 5.84204 7.86597C5.97304 7.47297 5.75997 7.04797 5.36597 6.91797C4.97297 6.78497 4.54897 7.00007 4.41797 7.39307C4.30697 7.72807 4.25 8.101 4.25 8.5V12.4299C4.25 13.3599 3.939 14.2779 3.375 15.0129C3.122 15.3419 3.18494 15.8119 3.51294 16.0649C3.64994 16.1689 3.80999 16.22 3.96899 16.22C4.19399 16.219 4.41697 16.118 4.56396 15.926ZM8.75 13.499V9.49902C8.75 9.08502 8.414 8.74902 8 8.74902C7.586 8.74902 7.25 9.08502 7.25 9.49902V13.499C7.25 13.913 7.586 14.249 8 14.249C8.414 14.249 8.75 13.913 8.75 13.499Z" fill="#0C071D"/>
+                </svg>
+                <p className='text-black ml-5 md:ml-5'>Signature</p>
+            </div>
+            <div className="bg-[#0C071D] bg-opacity-10 h-[31px] w-[228px] ml-12 md:ml-9 xl:ml-3 rounded-full -mt-1 items-center justify-center flex">
                 <p className='text-black text-[12px]'>304502203f004eeed0cef2fd...</p>
             </div>
         </div>
@@ -51,8 +58,8 @@ const ScriptContent: React.FC = () => {
             <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.5 0.75C9.778 0.75 6.75 3.778 6.75 7.5C6.75 8.015 6.8081 8.52309 6.9231 9.01709L0.969971 14.97C0.828971 15.111 0.75 15.302 0.75 15.5V19.5C0.75 19.914 1.086 20.25 1.5 20.25H5.5C5.914 20.25 6.25 19.914 6.25 19.5V17.25H8.5C8.699 17.25 8.89003 17.171 9.03003 17.03L11.9829 14.0769C12.4759 14.1919 12.984 14.25 13.5 14.25C17.222 14.25 20.25 11.222 20.25 7.5C20.25 3.778 17.222 0.75 13.5 0.75ZM13.5 12.75C12.979 12.75 12.467 12.6739 11.981 12.5229C11.714 12.4399 11.425 12.512 11.229 12.71L8.18896 15.75H5.5C5.086 15.75 4.75 16.086 4.75 16.5V18.75H2.25V15.811L8.29004 9.771C8.48604 9.574 8.55905 9.28502 8.47705 9.02002C8.32705 8.53402 8.25 8.02198 8.25 7.50098C8.25 4.60598 10.605 2.25098 13.5 2.25098C16.395 2.25098 18.75 4.60598 18.75 7.50098C18.75 10.396 16.395 12.75 13.5 12.75ZM15.77 6.5C15.77 7.19 15.21 7.75 14.52 7.75C13.831 7.75 13.2649 7.19 13.2649 6.5C13.2649 5.81 13.82 5.25 14.51 5.25H14.52C15.21 5.25 15.77 5.81 15.77 6.5Z" fill="#0C071D"/>
             </svg>
-            <p className='text-black ml-3 md:ml-4'>Public Key</p>
-            <div className="bg-[#0C071D] bg-opacity-10 h-[31px] w-[228px] ml-6 md:ml-8 lg:ml-3 rounded-full -mt-1 items-center justify-center flex">
+            <p className='text-black ml-5 md:ml-4'>Public Key</p>
+            <div className="bg-[#0C071D] bg-opacity-10 h-[31px] w-[228px] ml-11 md:ml-8 lg:ml-3 rounded-full -mt-1 items-center justify-center flex">
                 <p className='text-black text-[12px]'>304502203f004eeed0cef2fd...</p>
             </div>
         </div>
@@ -60,8 +67,8 @@ const ScriptContent: React.FC = () => {
             <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M13.5 0.75C9.778 0.75 6.75 3.778 6.75 7.5C6.75 8.015 6.8081 8.52309 6.9231 9.01709L0.969971 14.97C0.828971 15.111 0.75 15.302 0.75 15.5V19.5C0.75 19.914 1.086 20.25 1.5 20.25H5.5C5.914 20.25 6.25 19.914 6.25 19.5V17.25H8.5C8.699 17.25 8.89003 17.171 9.03003 17.03L11.9829 14.0769C12.4759 14.1919 12.984 14.25 13.5 14.25C17.222 14.25 20.25 11.222 20.25 7.5C20.25 3.778 17.222 0.75 13.5 0.75ZM13.5 12.75C12.979 12.75 12.467 12.6739 11.981 12.5229C11.714 12.4399 11.425 12.512 11.229 12.71L8.18896 15.75H5.5C5.086 15.75 4.75 16.086 4.75 16.5V18.75H2.25V15.811L8.29004 9.771C8.48604 9.574 8.55905 9.28502 8.47705 9.02002C8.32705 8.53402 8.25 8.02198 8.25 7.50098C8.25 4.60598 10.605 2.25098 13.5 2.25098C16.395 2.25098 18.75 4.60598 18.75 7.50098C18.75 10.396 16.395 12.75 13.5 12.75ZM15.77 6.5C15.77 7.19 15.21 7.75 14.52 7.75C13.831 7.75 13.2649 7.19 13.2649 6.5C13.2649 5.81 13.82 5.25 14.51 5.25H14.52C15.21 5.25 15.77 5.81 15.77 6.5Z" fill="#0C071D"/>
             </svg>
-            <p className='text-black ml-3 md:ml-4'>Hashed Key</p>
-            <div className="bg-[#0C071D] bg-opacity-10 h-[31px] w-[228px] ml-3 md:ml-5 xl:ml-3 rounded-full -mt-1 items-center justify-center flex">
+            <p className='text-black ml-5 md:ml-4'>Hashed Key</p>
+            <div className="bg-[#0C071D] bg-opacity-10 h-[31px] w-[228px] ml-8 md:ml-5 xl:ml-3 rounded-full -mt-1 items-center justify-center flex">
                 <p className='text-black text-[12px]'>304502203f004eeed0cef2fd...</p>
             </div>
         </div>
