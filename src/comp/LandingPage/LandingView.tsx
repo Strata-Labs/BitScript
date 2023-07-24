e666768610861ea814fefbc826f63b102936da53
Pato Gomez <pato2k5@gmail.com>
2023-07-24 14:44:13 -0600

    Landing Desktop View Start

--- src/comp/LandingPage/LandingView.tsx
+++ src/comp/LandingPage/LandingView.tsx
@@ -168,16 +168,19 @@ const ScriptsMenu = () => {
 
 const LandingView = () => {
   return (
-    <div className="flex flex-col bg-[#F8F8F8] w-screen h-screen items-center overflow-auto">
-      <div className="min-h-[213px] w-[353px] bg-[#0C071D] rounded-2xl mt-[100px] flex flex-col items-center justify-center">
-        <p className='gradient-text text-[31px] ml-5 mr-5 text-center font-semibold'> Learn & Write Bitcoin</p>
-        <button className='flex w-[221px] h-[44px] bg-white rounded-lg mt-5 items-center justify-center'>
-            <p className='text-black'> Open Script Sandbox</p>
-            <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.99986 16.4583C8.15986 16.4583 8.31988 16.3975 8.44155 16.275L14.2749 10.4417C14.519 10.1975 14.519 9.80164 14.2749 9.55747L8.44155 3.72414C8.19738 3.47997 7.80152 3.47997 7.55735 3.72414C7.31319 3.96831 7.31319 4.36417 7.55735 4.60834L12.949 9.99998L7.55735 15.3916C7.31319 15.6358 7.31319 16.0317 7.55735 16.2758C7.67985 16.3975 7.83986 16.4583 7.99986 16.4583Z" fill="#25314C"/>
-            </svg>
-        </button>
-      </div>
+    <div className="flex flex-col bg-[#F8F8F8] h-screen items-center overflow-auto md:items-start ml-0 md:ml-[270px]">
+        <div className='w-[100%]'>
+            <div className="min-h-[213px] bg-[#0C071D] rounded-2xl mt-[100px] flex flex-col items-center justify-center md:mt-[30px] md:min-w-[1156px] md:min-h-[114px] md:flex-row md:justify-between mr-[75px] ml-[75px] md:ml-0 md:mr-10">
+                <p className='gradient-text text-[31px] ml-5 mr-5 text-center font-semibold md:hidden flex'>Learn & Write Bitcoin</p>
+                <p className='gradient-text text-[31px] ml-5 mr-5 text-center font-semibold md:flex hidden'>Learn & Write Bitcoin Script</p>
+                <button className='flex w-[221px] h-[44px] bg-white rounded-lg mt-5 md:mt-0 items-center justify-center mr-5'>
+                    <p className='text-black'> Open Script Sandbox</p>
+                    <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.99986 16.4583C8.15986 16.4583 8.31988 16.3975 8.44155 16.275L14.2749 10.4417C14.519 10.1975 14.519 9.80164 14.2749 9.55747L8.44155 3.72414C8.19738 3.47997 7.80152 3.47997 7.55735 3.72414C7.31319 3.96831 7.31319 4.36417 7.55735 4.60834L12.949 9.99998L7.55735 15.3916C7.31319 15.6358 7.31319 16.0317 7.55735 16.2758C7.67985 16.3975 7.83986 16.4583 7.99986 16.4583Z" fill="#25314C"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
       <p className='text-black mt-10 font-semibold'>Review Common Scripts</p>
       <div className='mt-5'>
         <ScriptsMenu/>
