d7647e0dfec4667dea385c68f1e8ca53f5fcf784
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-08-13 13:29:29 -0400

    fix home page

---
+++ pages/TestCoreLib.tsx
@@ -0,0 +1,9 @@
+import TestingCoreLib from "@/comp/TestingCoreLib";
+
+export default function TEMP() {
+  return (
+    <div className="flex flex-col">
+      <TestingCoreLib />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import TestingCoreLib from "@/comp/TestingCoreLib";
+import LandingView from "@/comp/LandingPage/LandingView";
+import SearchView from "@/comp/SearchView/SearchView";
+import { activeSearchView } from "@/comp/atom";
+import { useAtom } from "jotai";
 
 export default function TEMP() {
-  return (
-    <div className="flex flex-col">
-      <TestingCoreLib />
-    </div>
-  );
+  const [showSearchView] = useAtom(activeSearchView);
+
+  return <div>{showSearchView ? <SearchView /> : <LandingView />}</div>;
 }
