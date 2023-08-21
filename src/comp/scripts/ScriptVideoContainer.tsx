cf2e55d42cad317f29d10777ec6e6f1529805587
Patotking12 <pato2k5@gmail.com>
2023-08-21 15:45:29 -0600

    Updated naming

--- src/comp/scripts/ScriptVideoContainer.tsx
+++ src/comp/scripts/ScriptVideoContainer.tsx
@@ -2,7 +2,7 @@ import { ScriptControl } from "@/SCRIPT_ANIMATION_LIB/ScriptControl";
 import React, { use, useEffect, useRef, useState } from "react";
 import { Line } from "rc-progress";
 
-import P2pkh from "@/const/SCRIPTS/P2PKH";
+import P2PKH from "@/const/SCRIPTS/P2PKH";
 
 import { useRouter } from "next/router";
 import { classNames, useIsMobile, useWindowSize } from "@/utils";
@@ -103,7 +103,7 @@ const BottomVideoContainer: React.FC = () => {
     setHeight(svgHeight);
 
     const scriptControlClass = new ScriptControl({
-      scriptStackSteps: P2pkh,
+      scriptStackSteps: P2PKH,
       width: svgWidth,
       height: svgHeight,
       autoPlay: true,
@@ -123,7 +123,7 @@ const BottomVideoContainer: React.FC = () => {
 
   const checkStep = (step: number) => {
     // check if step is less than the length of _TEST
-    if (step < P2pkh.length && step >= 0) {
+    if (step < P2PKH.length && step >= 0) {
       if (scriptHandler) {
         scriptHandler.goToStep(step);
       }
@@ -137,7 +137,7 @@ const BottomVideoContainer: React.FC = () => {
   };
 
   const goForwardStep = () => {
-    if (currentStep < P2pkh.length - 1) {
+    if (currentStep < P2PKH.length - 1) {
       checkStep(currentStep + 1);
     }
   };
@@ -231,7 +231,7 @@ const BottomVideoContainer: React.FC = () => {
 
             <div className="flex h-2 w-full items-center px-4 ">
               <Line
-                percent={100 - (P2pkh.length / currentStep) * 10}
+                percent={100 - (P2PKH.length / currentStep) * 10}
                 strokeWidth={0.5}
                 strokeColor="#000000"
               />
