bf199e0f061cc1b55a9620328e5a517ab81f61c9
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-08-21 19:16:15 -0400

    bug: naming

--- src/utils/SCRIPTS.ts
+++ src/utils/SCRIPTS.ts
@@ -1,5 +1,5 @@
 import { SCRIPTS_PAGE_PROPS } from "@/comp/scripts/p2pkh";
 
-import { P2PKH } from "@/const/SCRIPTS/P2PKH";
+import { P2PKH } from "@/const/SCRIPTS/p2pkh";
 
 export const SCRIPTS_LIST: SCRIPTS_PAGE_PROPS[] = [P2PKH];
