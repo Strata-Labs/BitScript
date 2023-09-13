c44ca87bbf1c3a7ab51b58bc364c1d9bd8369573
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-09-12 21:27:23 -0400

    chore: clean up lib for using with front end

--- src/deserialization/index.ts
+++ src/deserialization/index.ts
@@ -1,5 +1,49 @@
 import axios from "axios";
 
+// Error List
+const errInvalidInput = new Error(
+  "Invalid string length - TXID must be exactly 64 chars OR HEX transaction should be at least > 128 chars"
+);
+const errStringLengthInLE8 = new Error(
+  "Little Endian - input string must be 8 characters"
+);
+const errStringLengthInLE16 = new Error(
+  "Little Endian - input string must be 16 characters"
+);
+const errStringLengthInLE64 = new Error(
+  "Little Endian - Input string must be 64 characters"
+);
+const errIncompleteVarIntFC = new Error(
+  "Incomplete VarInt - byte decimal value > fc"
+);
+const errIncompleteVarIntFD = new Error(
+  "Incomplete VarInt - For prefix 'fd', 2 bytes are expected"
+);
+const errIncompleteVarIntFE = new Error(
+  "Incomplete VarInt - for prefix 'fe', 4 bytes are expected"
+);
+const errIncompleteVarIntFF = new Error(
+  "Incomplete VarInt - for prefix 'ff', 8 bytes are expected"
+);
+const errInvalidVersionEndian = new Error(
+  "Invalid version - endian format is incorrect, try in little endian"
+);
+const errNonstandardVersion = new Error(
+  "Nonstandard version - only 00000001 & 00000002 are mined *&* relayed"
+);
+
+// Known Scripts List
+enum KnownScript {
+  NONE = "NONE",
+  P2PKH = "P2PKH",
+  P2SH = "P2SH",
+  P2WPKH = "P2WPKH",
+  P2WSH = "P2WSH",
+  P2SHP2WPKH = "P2SH-P2WPKH",
+  P2SHP2WSH = "P2SH-P2WSH",
+  P2TR = "P2TR",
+}
+
 type VarInt = string;
 
 // Input Model & Helpers
@@ -11,6 +55,7 @@ interface TxInput {
   sigScript: string;
   sequence: string;
   isSegWit: boolean;
+  knownScipt?: KnownScript;
 }
 // Input Helpers
 
@@ -18,11 +63,13 @@ interface TxOutput {
   amount: number;
   pubKeySize: VarInt;
   pubKeyScript: string;
+  knownScript?: KnownScript;
 }
 
 interface TxWitness {
-  witnessNumElements: string;
+  witnessNumElements: number;
   witnessElements: TxWitnessElement[];
+  knownScript?: KnownScript;
 }
 
 interface TxWitnessElement {
@@ -30,7 +77,8 @@ interface TxWitnessElement {
   elementValue: string;
 }
 
-export interface TxData {
+interface TxData {
+  hash: string;
   txType: boolean;
   version: string;
   marker?: string;
@@ -51,30 +99,49 @@ interface MinTxData {
   witnesses?: TxWitness[];
   locktime: string;
 }
-async function fetchTXID(txid: string): Promise<string> {
-  if (txid.length !== 64) {
-    throw new Error("Invalid TXID length");
-  }
 
+//const errInvalidTXIDLength
+
+async function fetchTXID(txid: string): Promise<string> {
   // Actual API call here
   const response = await axios.get(`https://mempool.space/api/tx/${txid}/hex`);
   return response.data;
 }
 
+// Helper & Utility
+// VarInt
 function verifyVarInt(varint: string): VarInt {
   const firstTwoChars = varint.substring(0, 2);
+
   if (firstTwoChars === "fd") {
+    if (varint.length < 6) {
+      throw errIncompleteVarIntFD;
+    }
     return varint.substring(0, 6);
   }
+
   if (firstTwoChars === "fe") {
+    if (varint.length < 10) {
+      throw errIncompleteVarIntFE;
+    }
     return varint.substring(0, 10);
   }
+
   if (firstTwoChars === "ff") {
+    if (varint.length < 18) {
+      throw errIncompleteVarIntFF;
+    }
     return varint.substring(0, 18);
   }
+
+  if (parseInt(firstTwoChars, 16) >= parseInt("fd", 16)) {
+    throw errIncompleteVarIntFC;
+  }
+
   return varint.substring(0, 2);
 }
 
+// Hex <-> JSON
 function hexToJSON(hex: string): TxData {
   // Implement the logic to convert hex to JSON
   return {} as TxData;
@@ -85,9 +152,10 @@ function jsonToHex(json: TxData): string {
   return "";
 }
 
+// Little-Endian
 function leToBe8(le: string): string {
   if (le.length !== 8) {
-    throw new Error("Input string must be 8 characters");
+    throw errStringLengthInLE8;
   }
 
   let be = "";
@@ -97,10 +165,9 @@ function leToBe8(le: string): string {
   }
   return be;
 }
-
 function leToBe16(le: string): string {
   if (le.length !== 16) {
-    throw new Error("Input string must be 16 characters");
+    throw errStringLengthInLE16;
   }
 
   let be = "";
@@ -110,10 +177,9 @@ function leToBe16(le: string): string {
   }
   return be;
 }
-
 function leToBe64(le: string): string {
   if (le.length !== 64) {
-    throw new Error("Input string must be 64 characters");
+    throw errStringLengthInLE64;
   }
 
   let be = "";
@@ -123,12 +189,44 @@ function leToBe64(le: string): string {
   }
   return be;
 }
+// Categorization Functions
+// Categorize Output
+function parseOutputForKnownScript(
+  pubKeySize: string,
+  pubKeyScript: string
+): KnownScript {
+  if (pubKeyScript.slice(0, 4) === "0014") {
+    return KnownScript.P2WPKH;
+  } else if (pubKeyScript.slice(0, 2) === "a9") {
+    return KnownScript.P2SH;
+  } else {
+    throw new Error("todo");
+  }
+}
+// Categorize Witness
+function parseWitnessForKnownScript(
+  input: TxInput,
+  numElements: number,
+  elements: TxWitnessElement[]
+): KnownScript {
+  if (numElements === 2 && input.sigScriptSize === "00") {
+    return KnownScript.P2WPKH;
+  } else if (numElements === 2 && input.sigScriptSize === "17") {
+    return KnownScript.P2SHP2WPKH;
+  } else if (input.sigScriptSize !== "00" && numElements > 2) {
+    return KnownScript.P2SHP2WSH;
+  } else if (numElements === 1 && input.sigScriptSize === "00") {
+    return KnownScript.P2TR;
+  } else {
+    throw new Error("todo");
+  }
+}
 
+// Verification Functions
 function verifyLegacy(txData: string): {
   inputs: TxInput[];
   outputs: TxOutput[];
-  locktime: string;
-  sequence?: string;
+  sequence: string;
 } {
   // Inputs
   // Extract input count using VarInt
@@ -179,9 +277,7 @@ function verifyLegacy(txData: string): {
       isSegWit: isSegWitLocal,
     });
   }
-  //console.log(inputs);
-  //console.log(txDataNoInputCount);
-  //console.log(offset);
+
   // Outputs
   // Extract output count & size using VarInt
   const outputCountVarInt = verifyVarInt(
@@ -203,8 +299,6 @@ function verifyLegacy(txData: string): {
       txDataNoInputCount.slice(offset, 18 + offset)
     );
     const pubKeyVarIntSize = pubKeySize.length;
-    //console.log(pubKeySize);
-    //console.log(pubKeyVarIntSize);
     offset += pubKeyVarIntSize;
     // Parse script
     const pubKeyDecSize = parseInt(pubKeySize, 16);
@@ -223,54 +317,41 @@ function verifyLegacy(txData: string): {
   console.log(inputs);
   console.log(outputs);
   //console.log(txData.slice(-8));
-  return {
-    inputs: inputs,
-    outputs: outputs,
-    locktime: txData.slice(-8),
-  };
+  throw new Error("LEGACY TEMP ");
+  // return {
+  //   inputs: inputs,
+  //   outputs: outputs,
+  //   locktime: txData.slice(-8),
+  // };
 }
 
 function verifySegWit(txData: String): MinTxData {
-  console.log("verifySegWit - txData", txData);
   // Inputs
   // Extract input count using VarInt
   const inputCountVarInt = verifyVarInt(txData.slice(0, 18));
-  console.log("inputCountVarInt", inputCountVarInt);
   const inputCountVarIntSize = inputCountVarInt.length;
-  console.log("inputCountVarIntSize", inputCountVarIntSize);
   const inputCount = parseInt(inputCountVarInt, 16);
-  console.log("inputCount", inputCount);
   // Create empty array of Inputs
   const inputs: TxInput[] = [];
   const txDataNoInputCount = txData.slice(inputCountVarIntSize);
-
   //console.log(txDataNoInputCount);
   // Loop through transaction inputCountVarInt amount of times to extract inputs
   let offset = 0;
   for (let i = 0; i < inputCount; i++) {
     // Parse next 64 characters & change from LE to BE -> TXID
-    console.log("txId", txDataNoInputCount.slice(0 + offset, 64 + offset));
     const txid = leToBe64(txDataNoInputCount.slice(0 + offset, 64 + offset));
     offset += 64;
     // Parse next 8 characters & change from LE to BE -> VOUT
-    console.log("vout", txDataNoInputCount.slice(0 + offset, 8 + offset));
     const vout = leToBe8(txDataNoInputCount.slice(0 + offset, 8 + offset));
-    console.log("vout leToBe8", vout);
     //console.log("after suspected vout");
     offset += 8;
     // Parse up to next 10 characters for sigScriptSize
     const scriptSigSize = verifyVarInt(
       txDataNoInputCount.slice(offset, 18 + offset)
     );
-
-    console.log("scriptSigSize", scriptSigSize);
     const scriptSigSizeSize = scriptSigSize.length;
-    console.log("scriptSigSizeSize", scriptSigSizeSize);
-
     const scriptSigSizeInt = parseInt(scriptSigSize, 16);
-    console.log("scriptSigSizeInt", scriptSigSizeInt);
     offset += scriptSigSizeSize;
-
     let scriptSig = "";
     let isSegWitLocal = false;
     if (scriptSigSize === "00") {
@@ -294,8 +375,6 @@ function verifySegWit(txData: String): MinTxData {
       isSegWit: isSegWitLocal,
     });
   }
-  //console.log(offset)
-  //console.log(txData.slice(offset));
 
   // Outputs
   // Extract output count & size using VarInt
@@ -318,8 +397,6 @@ function verifySegWit(txData: String): MinTxData {
       txDataNoInputCount.slice(offset, 18 + offset)
     );
     const pubKeyVarIntSize = pubKeySize.length;
-    //console.log(pubKeySize);
-    //console.log(pubKeyVarIntSize);
     offset += pubKeyVarIntSize;
     // Parse script
     const pubKeyDecSize = parseInt(pubKeySize, 16);
@@ -333,11 +410,11 @@ function verifySegWit(txData: String): MinTxData {
       amount: parseInt(amount, 16),
       pubKeySize: pubKeySize,
       pubKeyScript: pubKeyScript,
+      knownScript: parseOutputForKnownScript(pubKeySize, pubKeyScript),
     });
   }
   console.log(inputs);
   console.log(outputs);
-  console.log(txDataNoInputCount.slice(offset));
   // Create empty array of Witnesses
   const witnesses: TxWitness[] = [];
   for (let i = 0; i < inputCount; i++) {
@@ -364,15 +441,22 @@ function verifySegWit(txData: String): MinTxData {
         elementSize: elementVarInt,
         elementValue: elementValue,
       });
+      //console.log(elementValue);
     }
+    //console.log("from inside of witness loop: " + JSON.stringify(inputs[i]));
     //console.log(witnessElements);
     witnesses.push({
-      witnessNumElements: witnessNumOfElementsInt + "",
+      witnessNumElements: witnessNumOfElementsInt,
       witnessElements: witnessElements,
+      knownScript: parseWitnessForKnownScript(
+        inputs[i],
+        witnessNumOfElementsInt,
+        witnessElements
+      ),
     });
   }
-  console.log(witnesses);
-  console.log(txData.slice(-8));
+  console.log(JSON.stringify(witnesses));
+
   return {
     inputCount: inputCountVarInt,
     inputs: inputs,
@@ -381,10 +465,8 @@ function verifySegWit(txData: String): MinTxData {
     witnesses: witnesses,
     locktime: txData.slice(-8),
   };
+  //  return true;
 }
-// More SegWit verification here
-
-//  return true;
 
 function updateTXData(txData: TxData, isLegacy: boolean): TxData {
   if (isLegacy) {
@@ -396,12 +478,16 @@ function updateTXData(txData: TxData, isLegacy: boolean): TxData {
 }
 
 function verifyAndUpdateTXDataHex(hexData: string): TxData {
-  //console.log(hexData);
   // Parsing for version
   const version = hexData.slice(0, 8);
   // Assert version is valid
   if (version !== "01000000" && version !== "02000000") {
-    throw new Error("Invalid version");
+    if (version.slice(0, 2) === "00") {
+      throw errInvalidVersionEndian;
+    }
+    if (parseInt(version) >= 3) {
+      throw errNonstandardVersion;
+    }
   }
   // Parsing to check for Marker & Flag field -> means it's SegWit
   const isSegWit = hexData.slice(8, 12) === "0001" ? true : false;
@@ -413,88 +499,56 @@ function verifyAndUpdateTXDataHex(hexData: string): TxData {
     const hexDataSegWit = hexData.slice(12);
     //console.log(hexDataSegWit);
     const SegWitRes = verifySegWit(hexDataSegWit);
+
     return {
+      hash: hexData,
       txType: true,
       version: hexData.slice(0, 8),
       ...SegWitRes,
     };
   } else {
-    throw new Error("Not SegWit");
-  }
-  /*
-  comment this out in favor of returing a txData to the front end and OG Caller
-  else {
     // Store as Legacy
     const hexDataLegacy = hexData.slice(8);
     // Validate as Legacy
     verifyLegacy(hexDataLegacy);
+    throw new Error("FIX LEGACY TX RETURN VALUE");
   }
-  */
-  //up  dateTXData(txData, version === "01000000");
 }
 
-/* 
-  02000000000101634ca4909db158237af26bebbd28cf3b551f5ed4c5998bbff5692d4a21bad6220600000000fdffffff05303e19000000000016001419cd05e452b9bc7b4bbe9ffd359ff878102c4ed4ac010200000000001600146615380a87e9fcb9017a916593ca1d6ead71259da42e65000000000017a9146d4e008c341aa96d0a9fcba6c6180076493bfd4287d60916000000000017a914e63d1c7eff86591e317cacbad7353ae461559d908789a5a70100000000160014f60834ef165253c571b11ce9fa74e46692fc5ec102483045022100f06815aa20a790b286e45c94c6b9b7a4c3eb945555223e50c6171a4fe3e1d80502202291ffe91e4f90c381322dd41cab5a28a53657dfcd63c0cddca96cee298aa6d00121026e5628506ecd33242e5ceb5fdafe4d3066b5c0f159b3c05a621ef65f177ea28600000000
-*/
-const TEST_DESERIALIZE = async (txId: string) => {
+const TEST_DESERIALIZE = async (userInput: string) => {
   try {
     // SegWit/NotTapRoot -> 1 input | 5 outputs | 1 witness
     // f8622f0427425f769069e36f7fdfbde2a9d51ad44b6eef51435f24236de05239
     // SegWit -> 3 inputs | 2 outputs | 1 witness
     // b55b1886d1cecf733a12bcdcc8ef413f158d84eb4f75052abde2469fa3a004cd
-    // Legacy -> 3 inputs | 2 outputs
     // de0465c042cc00b46983c13e884f9a54e06d6f5ef3baf4c73238ed0ed70905ab
     // SegWit/Taproot ->
     // f73ce97a5b8b1a2c23d97b3e6aaf3d0af52bf94c28162f1a2f5d6bfb3c019a42
-    // const txid =
-    //   "b55b1886d1cecf733a12bcdcc8ef413f158d84eb4f75052abde2469fa3a004cd";
-    const hexData = await fetchTXID(txId);
-    console.log("hex data", hexData);
-    const isValid = verifyAndUpdateTXDataHex(hexData);
-    return isValid;
-    console.log(`Transaction is ${isValid ? "valid" : "invalid"}`);
+    // e0b46bf5838ed82946aa2d55986791885f26c890a1e9f341f584a3f1b4cb01da
+
+    // Assert that it's at least likely to be one a txid or hex
+    // High-level error check for string length
+    //const userInput = "e0b46bf5838ed82946aa2d55986791885f26c890a1e9f341f584a3f1b4cb01da"
+
+    if (userInput.length != 64 && userInput.length < 256) {
+      throw errInvalidInput;
+    }
+
+    // User submitted a TXID -> fetch -> store
+    if (userInput.length == 64) {
+      // Fetch hex of transaction
+      const hexData = await fetchTXID(userInput);
+      const isValid = verifyAndUpdateTXDataHex(hexData);
+      return isValid;
+      // User submitted a raw hex -> validate -> store
+    } else {
+      // Parse/Validate hex of transaction
+      const isValid = verifyAndUpdateTXDataHex(userInput);
+      return isValid;
+    }
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
-    throw new Error(error.message);
   }
 };
 
 export default TEST_DESERIALIZE;
-
-// Script categorization
-
-// String must be exactly 64 hex chars to fetch TXID
-// String must be v (8) + ic (2) + txid (64) + vout (8) + scriptSigSiz (2) + scriptSig (1) + sequence (8) + oc (4) + amount (16) + pubKeySize (2) + pubKey (1) + locktime (8)
-// String must be > 128 characters before checking for raw hex
-
-// Error Messages
-// Universal Issues
-//   Incorrect hex format
-// Version Issues
-//   Incorrect length -> String isn't at least
-//   Incorrect version -> Version should be either 01000000 or 02000000
-//   Incorrect endian format -> In big endian, should be little endian format
-// Input Count Issues
-//   Forbidden first two bytes -> VarInt can't be only "fd" "fe" or "ff"
-// Input TXID Issues
-//   Incorrect length
-//   TXID not found -> TXID not found, make sure it's in little endian format
-// Input VOUT Issues
-//  Incorrect length
-//  Incorrect endian format ->
-// Input ScriptSigSize Issues
-// Input ScriptSig Issues
-// Input Sequence Issues
-// Output Count Issues
-//   Forbidden first two bytes -> VarInt can't be only "fd" "fe" or "ff"
-// Output Amount Issues
-//  Invalid endian format
-//
-// Output PubKeyScriptSize Issues
-// Output PubKeyScript Issues
-// Witness WitnessSigSize Issues
-// Witness WitnessElementSize Issues
-// Witness WitnessElementItem Issues
-//
-// Locktime Issues
-//   Invalid Hex -> Invalid hex value
