2bf8ad3292b00c26cc7723bc1fdc5ec91e1d466e
jesus <jnajera1917@gmail.com>
2023-09-16 08:23:12 -0400

    started json response refactor

--- src/deserialization/model.ts
+++ src/deserialization/model.ts
@@ -5,10 +5,49 @@ import {
     KnownScript
   } from "./helpers";
 
-// Types of Transactions
+// Universal
+// Regardless of response type (hex | json) these types persist
 export enum TxType {
-    LEGACY = "Legacy",
-    SEGWIT = "SegWit",
+  LEGACY = "Legacy",
+  SEGWIT = "SegWit",
+}
+
+// Hex Response
+// All data required for when a user is looking at the hex / overlay view
+// The most important item is the parsedRaw which is an array of [transactionElements]
+export interface HexResponse {
+  txID: string;
+  rawHex: string;
+  txType: TxType;
+  numInputs: number;
+  numOutputs: number;
+  totalBitcoin: number;
+  knownScripts: KnownScript[];
+  parsedRawHex: TransactionItem[]; 
+}
+
+export interface TransactionItem {
+  error?: Error;
+  rawHex: string;
+  item: any;
+}
+
+export interface BaseTransactionItem {
+  title: string;
+  value: string;
+  description: string;
+}
+
+// JSON Response
+// All data required for when a user is looking at the JSON view
+// Types of Transactions
+
+export interface VersionItem extends BaseTransactionItem {
+  bigEndian: string;
+}
+
+export interface CountItem extends BaseTransactionItem {
+  asset: string;
 }
 
 // Input Model
