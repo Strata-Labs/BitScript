8c7527053e9ab6758d664d1e0e998ab2c795852a
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-09-21 00:52:25 -0400

    clean up table

--- src/comp/Transactions/PopUpSections/InputCount.tsx
+++ src/comp/Transactions/PopUpSections/InputCount.tsx
@@ -29,6 +29,64 @@ export const SIZE_DETAILS = [
     description: "ff + 8 bytes (in little-endian).",
   },
 ];
+
+export const SizeTable = () => {
+  return (
+    <div className="mx-4 mt-6 overflow-hidden  ring-1 ring-black  sm:rounded-lg">
+      <table className="min-w-full divide-y divide-[#F79327]">
+        <thead className="">
+          <tr>
+            <th
+              scope="col"
+              className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
+            >
+              Size
+            </th>
+            <th
+              scope="col"
+              className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
+            >
+              Rule
+            </th>
+            <th
+              scope="col"
+              className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
+            >
+              Example
+            </th>
+            <th
+              scope="col"
+              className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
+            >
+              Description
+            </th>
+            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+              <span className="sr-only">Edit</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody className=" bg-white">
+          {SIZE_DETAILS.map((deet, index) => (
+            <tr key={index}>
+              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-lg  text-black sm:pl-6">
+                {deet.size}
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
+                {deet.rules}
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
+                {deet.examples}
+              </td>
+              <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
+                {deet.description}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 const InputCount = (props: CountItem) => {
   return (
     <>
@@ -37,59 +95,7 @@ const InputCount = (props: CountItem) => {
         to fetch & unlock the Bitcoin spent in this transaction. It’s stored as
         a VarInt.
       </p>
-      <div className="mx-4 mt-6 overflow-hidden  ring-1 ring-black  sm:rounded-lg">
-        <table className="min-w-full divide-y divide-[#F79327]">
-          <thead className="">
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Size
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Rule
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Example
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Description
-              </th>
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody className=" bg-white">
-            {SIZE_DETAILS.map((deet, index) => (
-              <tr key={index}>
-                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-lg  text-black sm:pl-6">
-                  {deet.size}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
-                  {deet.rules}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
-                  {deet.examples}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
-                  {deet.description}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <SizeTable />
       <p className="mx-5 mt-5 text-lg text-[#0C071D]">
         With our input count, we know how many inputs we expect in the upcoming
         hex, recall that each input requires the following fields: TXID, VOUT,
@@ -109,59 +115,7 @@ export const OutPutCount = (props: CountItem) => {
         used to assign & lock the inputs spent. Like most items of varying size,
         it’s stored according to VarInt rules:
       </p>
-      <div className="mx-4 mt-6 overflow-hidden  ring-1 ring-black  sm:rounded-lg">
-        <table className="min-w-full divide-y divide-[#F79327]">
-          <thead className="">
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Size
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Rule
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Example
-              </th>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-2xl font-bold text-gray-900 sm:pl-6"
-              >
-                Description
-              </th>
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody className=" bg-white">
-            {SIZE_DETAILS.map((deet, index) => (
-              <tr key={index}>
-                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-lg  text-black sm:pl-6">
-                  {deet.size}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
-                  {deet.rules}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
-                  {deet.examples}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-lg text-black">
-                  {deet.description}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <SizeTable />
       <p className="mx-5 mt-5 text-lg text-[#0C071D]">
         With our output count, we know how many outputs we expect in the
         upcoming hex, recall that each output requires the following fields:
