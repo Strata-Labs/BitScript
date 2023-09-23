3c5ba49ea2f5733496e8b5f0609d58fed6bfcb45
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-09-23 12:39:56 -0400

    mobile transaction input view

--- src/comp/Transactions/TransactionInputView.tsx
+++ src/comp/Transactions/TransactionInputView.tsx
@@ -26,32 +26,33 @@ const TransactionInputView = ({
         <div className="ml-5 mt-5 font-extralight text-[#6C5E70] md:mt-0">
           <p>Transactions</p>
         </div>
-        <div className="mx-5 mt-2 font-light text-[#6C5E70]">
-          <p>
-            A Bitcoin transaction describes the flow of Bitcoin. Ultimately, a
-            Bitcoin block is just many verified transactions & the blockchain
-            itself is just a linked list of these blocks -{" "}
-            <span className="font-bold">
-              {" "}
-              which makes transactions the crux of Bitcoin.
-            </span>{" "}
-          </p>
-          <span className="mt-5">
+        <div className=" mx-5 mt-2 font-light text-[#6C5E70]">
+          <div className="hidden md:block">
             <p>
-              Below are two tools to{" "}
-              <span className="font-semibold text-[#F79327]">
-                read/deserialize/parse
+              A Bitcoin transaction describes the flow of Bitcoin. Ultimately, a
+              Bitcoin block is just many verified transactions & the blockchain
+              itself is just a linked list of these blocks -{" "}
+              <span className="font-bold">
+                {" "}
+                which makes transactions the crux of Bitcoin.
               </span>{" "}
-              or to{" "}
-              <span className="font-semibold text-[#F79327]">
-                write/serialize/create
-              </span>{" "}
-              a transaction.
             </p>
-          </span>
-
-          <div className="mt-5 flex flex-col justify-between md:flex-row">
-            <p className="text-[30px] font-semibold text-[#0C071D] md:text-[38px]">
+            <span className="mt-5">
+              <p>
+                Below are two tools to{" "}
+                <span className="font-semibold text-[#F79327]">
+                  read/deserialize/parse
+                </span>{" "}
+                or to{" "}
+                <span className="font-semibold text-[#F79327]">
+                  write/serialize/create
+                </span>{" "}
+                a transaction.
+              </p>
+            </span>
+          </div>
+          <div className="mt-2  flex  flex-row items-center justify-between md:mt-5 md:flex-row">
+            <p className="text-lg font-semibold text-[#0C071D] md:text-2xl md:text-[38px]">
               Deserialize A Transaction
             </p>
             <ModularButton txInputType={txInputType} />
@@ -89,7 +90,7 @@ const TransactionInputView = ({
               <textarea
                 onChange={handleTextAreaChange}
                 placeholder="paste in a raw hex, json, transaction ID, or  load an example above"
-                className="mt-5 h-[240px] w-full rounded-2xl border border-transparent bg-[#F0F0F0] p-10"
+                className="mt-5 h-[240px] w-full rounded-2xl border border-transparent bg-[#F0F0F0] px-8 py-4 md:p-10"
                 value={txUserInput}
               ></textarea>
             </>
