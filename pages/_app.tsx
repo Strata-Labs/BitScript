dccdd1ec43cb5381ef909ff4f01a5d7f312ceef0
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-09-27 09:24:03 -0400

    add latest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <PlausibleProvider
       trackOutboundLinks={true}
       taggedEvents={true}
-      domain="bitscript.vercel.app "
+      domain="bitscript.app"
       trackLocalhost={false}
     >
       <Provider>
