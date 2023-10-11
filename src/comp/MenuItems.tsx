c38c95925f8f8dc53f9004aa8dbbb311ea79a3dc
Patotking12 <pato2k5@gmail.com>
2023-10-10 19:58:07 -0600

    Bug Fix

--- src/comp/MenuItems.tsx
+++ src/comp/MenuItems.tsx
@@ -163,7 +163,7 @@ const Menu = () => {
           </Link> */}
         </div>
         {/* About */}
-        <div className="mb-[100px] flex flex-col md:mb-10 md:mt-auto">
+        <div className="mb-[130px] flex flex-col md:mb-10 md:mt-auto">
           <Link
             href="/about"
             className={getItemClassName("about")}
