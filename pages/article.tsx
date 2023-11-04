1d3b33cfb6391925ac3d3693fbb9f7095bc1ee83
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-11-03 21:22:24 -0400

    chore dynamic url for lessons

--- pages/article.tsx
+++ pages/article.tsx
@@ -2,9 +2,18 @@ import SearchView from "@/comp/SearchView/SearchView";
 import { activeSearchView } from "@/comp/atom";
 import { useAtom } from "jotai";
 import ArticleView from "@/comp/Tutorials/ArticleView";
+import { BitcoinBasicsTutorials } from "@/utils/TUTORIALS";
 
 export default function TEMP() {
   const [showSearchView] = useAtom(activeSearchView);
 
-  return <div>{showSearchView ? <SearchView /> : <ArticleView />}</div>;
+  return (
+    <div>
+      {showSearchView ? (
+        <SearchView />
+      ) : (
+        <ArticleView {...BitcoinBasicsTutorials[0]} />
+      )}
+    </div>
+  );
 }
