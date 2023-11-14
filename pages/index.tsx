037c6a06f5e0cabda581f261c40a778da2316a2c
Patotking12 <pato2k5@gmail.com>
2023-11-14 09:36:56 -0600

    Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import LandingView from "@/comp/LandingPage/LandingView";
+import LandingPage from "@/comp/MainPage/LandingPage";
 import SearchView from "@/comp/SearchView/SearchView";
 import { activeSearchView, menuOpen } from "@/comp/atom";
 import { useAtom } from "jotai";
 
 export default function TEMP() {
-  const [showSearchView] = useAtom(activeSearchView);
   const [isMenuOpen, setMenuOpen] = useAtom(menuOpen);
 
   if (isMenuOpen === true) {
     return null;
   }
 
-  return <div>{showSearchView ? <SearchView /> : <LandingView />}</div>;
+  return <div>{<LandingPage />}</div>;
 }
