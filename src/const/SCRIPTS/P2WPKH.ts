f2135dc8de6545135843d96ea84c6b310c872fc5
Patotking12 <pato2k5@gmail.com>
2023-11-30 21:42:05 -0600

    Scripts

--- src/const/SCRIPTS/P2WPKH.ts
+++ src/const/SCRIPTS/P2WPKH.ts
@@ -9,42 +9,47 @@ import { P2PK_STEPS } from "./p2pk";
 
 const codeBlocks: CodeBlockType[] = [
   {
-    code: "# UnlockScript/LockScript",
+    code: "# WitnessScript",
     displayType: CodeDisplayBlock.comment,
   },
   {
-    code: "[lock-script]",
+    code: "<OP_70 - 72> ",
     displayType: CodeDisplayBlock.code,
     step: 0,
   },
   {
-    code: "[unlock-script]",
+    code: "[ecdsa-signature]",
     displayType: CodeDisplayBlock.code,
     step: 1,
+  },
+  {
+    code: "<OP_33> ",
+    displayType: CodeDisplayBlock.code,
+    step: 2,
+  },
+  {
+    code: "[public-key]",
+    displayType: CodeDisplayBlock.code,
+    step: 3,
   },
   {
     code: "# PubKeyScript",
     displayType: CodeDisplayBlock.comment,
   },
   {
-    code: "<OP_HASH160> ",
-    displayType: CodeDisplayBlock.code,
-    step: 2,
-  },
-  {
-    code: "<OP_20>",
-    displayType: CodeDisplayBlock.code,
-    step: 3,
-  },
-  {
-    code: "[hash160 [lock-script]]",
+    code: "<OP_0> ",
     displayType: CodeDisplayBlock.code,
     step: 4,
   },
   {
-    code: "<OP_EQUAL> ",
+    code: "<OP_20>",
     displayType: CodeDisplayBlock.code,
     step: 5,
+  },
+  {
+    code: "[hash160 [public-key]]",
+    displayType: CodeDisplayBlock.code,
+    step: 6,
   },
 ];
 
@@ -54,16 +59,17 @@ const P2WPKH: SCRIPTS_PAGE_PROPS = {
   STACK_DATA: P2PK_STEPS,
   shortHand: "P2WPKH",
   longHand: "(pay to witness public key hash)",
-  shortDescription: "",
+  shortDescription:
+    "The standard SegWit script for  direct transfers & the most common transaction type.",
   longDescription: [
     " A Pay-to-Witness-Public-Key-Hash (P2WPKH) script is the standard SegWit script for a direct transfer transaction. The logic for a P2WPKH witness script & output pubKeyScript is exactly the same as a P2PKH input sigScript & output pubKeyScript. However, there is a large difference in the op_codes explicitly expressed in the raw transaction. Wallets/clients know that when a P2WPKH output is detected they'll need to insert the common P2PKH op_codes; therefore, a P2WPKH pubKeyScript requires only one specific item: a hashed public key.",
   ],
 
-  introduction: "BIP133",
+  introduction: "BIP141",
   opCodeReview:
     "P2WPKH requires the same three (3) pieces of data & four (4) op_codes as P2PKH. However, as we see in the first step below, the initial formatting for the ScriptPubKey is different.",
   inUse: "Yes",
-  numberOfOps: "14",
+  numberOfOps: "",
   generalType: "Script",
   linkPath: "/scripts/P2WPKH",
   exampleLink: "",
--- src/utils/SCRIPTS.ts
+++ src/utils/SCRIPTS.ts
@@ -1,7 +1,15 @@
 import { SCRIPTS_PAGE_PROPS } from "@/comp/scripts/ScriptView";
 import P2SH from "@/const/SCRIPTS/P2SH";
+import P2WPKH from "@/const/SCRIPTS/P2WPKH";
+import P2WSH from "@/const/SCRIPTS/P2WSH";
 import P2PK from "@/const/SCRIPTS/p2pk";
 
 import { P2PKH } from "@/const/SCRIPTS/p2pkh";
 
-export const SCRIPTS_LIST: SCRIPTS_PAGE_PROPS[] = [P2PK, P2PKH, P2SH];
+export const SCRIPTS_LIST: SCRIPTS_PAGE_PROPS[] = [
+  P2PK,
+  P2PKH,
+  P2SH,
+  P2WPKH,
+  P2WSH,
+];
