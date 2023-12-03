aa901bcc8cd59743645fbbcc8b2a0574bc71b377
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-12-03 17:13:50 -0500

    folde clean up

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -13,18 +13,7 @@ import {
   createStripeCustomerPortal,
   fetchPayment,
 } from "./payment";
-import {
-  checkLessonCompletionStatus,
-  fetchUserLessons,
-  completeLessonEvent,
-  createHistoryEvent,
-  createLessonEvent,
-  fetchUserHistory,
-  fetchOrAddIPAddress,
-  updateQueryCountForIPAddress,
-  fetchOrAddUserQuery,
-  updateUserQueryCount,
-} from "./userHistory";
+import { createHistoryEvent, fetchUserHistory } from "./userHistory";
 import { sendEmailText } from "./email";
 import {
   createSandboxScriptEvent,
@@ -32,6 +21,18 @@ import {
   fetchUserSandboxScripts,
   updateSandboxScriptEvent,
 } from "./userSandboxScripts";
+import {
+  checkLessonCompletionStatus,
+  completeLessonEvent,
+  createLessonEvent,
+  fetchUserLessons,
+} from "./lessons";
+import {
+  fetchOrAddIPAddress,
+  fetchOrAddUserQuery,
+  updateQueryCountForIPAddress,
+  updateUserQueryCount,
+} from "./userAccess";
 
 export const appRouter = router({
   // user procedures
@@ -54,7 +55,7 @@ export const appRouter = router({
   // email procedures
   sendEmailText: sendEmailText,
 
-  // save progress
+  // User Lessons
   createLessonEvent: createLessonEvent,
   completeLessonEvent: completeLessonEvent,
   checkLessonCompletionStatus: checkLessonCompletionStatus,
