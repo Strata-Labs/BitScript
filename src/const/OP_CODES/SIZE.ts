d976b2e5e2b54273465a550d3dce440098652705
Patotking12 <pato2k5@gmail.com>
2023-12-03 17:39:43 -0600

    Hash256 Return Sha1 Size Sub Swap Whithin

--- src/const/OP_CODES/SIZE.ts
+++ src/const/OP_CODES/SIZE.ts
@@ -8,7 +8,249 @@ import {
 import { OP_CODE_PAGE_PROPS } from "@/comp/opCodes/OpCodeView";
 import SHA1 from "@/../public/images/SHA1.svg";
 
-const SIZE_STEPS: EXECUTION_STEPS[] = [];
+const SIZE_STEPS: EXECUTION_STEPS[] = [
+  // Video 1
+  {
+    containers: [0],
+    mainStack: [],
+    resultStack: [],
+    actions: [
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.MAIN_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          stackIndex: 0,
+          dataHex: "01000000",
+
+          dataNumber: "2",
+          className: "COLUMN-0-0",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+        },
+      },
+    ],
+  },
+  // Video 2
+  {
+    containers: [1],
+    mainStack: [
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        stackIndex: 0,
+        dataHex: "01000000",
+        dataNumber: "2",
+
+        className: "COLUMN-1-0",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        stackIndex: 0,
+        dataHex: "01000000",
+        dataNumber: "2",
+        className: "COLUMN-1-1",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.DUPLICATE,
+      },
+    ],
+    resultStack: [],
+    actions: [
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 0,
+        data: {
+          name: "OP_SIZE",
+          number: 130,
+          hex: "0x82",
+          stackIndex: 0,
+          description: ".",
+          className: "COLUMN-2-0",
+          libDataType: LIB_DATA_TYPE.OP_CODE,
+        },
+      },
+      {
+        moveType: MOVE_TYPE.MOVE_POP_ARROW,
+        to: COLUMN_TYPE.RESULT_STACK,
+
+        stackIndex: 1,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          dataHex: "01000000",
+          dataNumber: "2",
+          stackIndex: 1,
+          className: "COLUMN-1-1",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+        },
+      },
+    ],
+  },
+  // Video 3
+  {
+    containers: [],
+    mainStack: [
+      {
+        name: "OP_SIZE",
+        number: 130,
+        hex: "0x82",
+        description: "",
+        className: "COLUMN-1-0",
+        libDataType: LIB_DATA_TYPE.OP_CODE,
+        stackIndex: 0,
+      },
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        dataHex: "01000000",
+        dataNumber: 2,
+        stackIndex: 1,
+        className: "COLUMN-1-1",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.DUPLICATE,
+      },
+    ],
+    resultStack: [],
+    actions: [
+      {
+        moveType: MOVE_TYPE.ADD_EQUAL,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          dataHex: "01000000",
+          dataNumber: 1,
+          stackIndex: 0,
+          className: "COLUMN-2-0",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+          styleType: SCRIPT_DATA_STYLE_TYPE.EQUAL,
+        },
+      },
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          dataHex: "01000000",
+          dataNumber: "Calculated Size",
+          stackIndex: 0,
+          className: "COLUMN-2-0",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+        },
+      },
+    ],
+  },
+  // Video 4
+  // Video 5
+  {
+    containers: [2],
+    mainStack: [
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        dataHex: "01000000",
+        dataNumber: "Calculated Size",
+        stackIndex: 0,
+        className: "COLUMN-1-0",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+    ],
+    resultStack: [
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        dataHex: "01000000",
+        dataNumber: 2,
+        stackIndex: 0,
+        className: "COLUMN-1-0",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+    ],
+    actions: [
+      {
+        moveType: MOVE_TYPE.MOVE_POP_ARROW,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 1,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          stackIndex: 1,
+          dataHex: "01000000",
+          dataNumber: "Calculated Size",
+          className: "COLUMN-1-0",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+        },
+      },
+    ],
+  },
+];
 
 export const OP_SIZE: OP_CODE_PAGE_PROPS = {
   name: "OP_SIZE",
@@ -35,6 +277,6 @@ export const OP_SIZE: OP_CODE_PAGE_PROPS = {
     failureSteps: SIZE_STEPS,
     title: "OP_Code Walkthrough",
     description: "Pushes the size of the top item on the stack onto the stack.",
-    steps: ["Peek at Top Item", "Push Size of Item"],
+    steps: ["Peek at Top Item", "Calculate Size of Item", "Push Size of Item"],
   },
 };
