01fa7945110a4c845d417dfeeb4d49aa8d4af7b8
Patotking12 <pato2k5@gmail.com>
2023-12-08 15:48:36 -0600

    About Section Complete

--- src/comp/About/About.tsx
+++ src/comp/About/About.tsx
@@ -3,11 +3,14 @@ import FormAbout from "./Form";
 import LogoWBg from "./LogoWBg";
 import PfpTeam from "./PfpTeam";
 import Link from "next/link";
+import { useAtom } from "jotai";
+import { teamMemberAtom } from "../atom";
 
 const About = () => {
   const createTeam = trpc.createTeam.useMutation();
   const createTeamUserLink = trpc.createTeamUserLink.useMutation();
   const sendEmailText = trpc.sendEmailText.useMutation();
+  const [hoveredImage, setHoveredImage] = useAtom(teamMemberAtom);
 
   const handleClick = async () => {
     try {
@@ -15,12 +18,80 @@ const About = () => {
       console.log(error);
     }
   };
+  const renderTeamMemberInfo = () => {
+    if (hoveredImage === "SetZeus") {
+      return (
+        <div className="h-[280px]">
+          <p className="text-right text-3xl font-medium">SetZeus</p>
+          <p className="mt-5 text-right font-extralight">
+            Founder/CEO with a decade experience in scaling a dev agency. Most
+            recently worked with TrustMachines & the Stacks ecosystem,
+            programming the Clarity smart contracts for the sBTC bridge. My
+            current contributions at the moment fall into Bitcoin development &
+            product design.
+          </p>
+          <p className="mt-5 text-right font-extralight">
+            Fun Fact: I researched & summarized the history of polymaths with a
+            focus on their 20s (google "Young Polymaths series")
+          </p>
+          <p className="mt-5 text-right font-bold">@setzeus</p>
+        </div>
+      );
+    } else if (hoveredImage === "SetBern") {
+      return (
+        <div className="h-[280px]">
+          <p className="text-right text-3xl font-medium">SetBern</p>
+          <p className="mt-5 text-right font-extralight">
+            Since 2016 Iʼve run a development agency as a co-founder & lead
+            React/Typescript/full-stack engineer. With a knack for starting
+            projects and not finishing them I'm grateful for the ones that see
+            the light of day against all odds. Currently building on Bitcoin &
+            Stacks one block at time.
+          </p>
+          <p className="mt-5 text-right font-bold">@setbern_</p>
+        </div>
+      );
+    } else if (hoveredImage === "SetPato") {
+      return (
+        <div className="h-[280px]">
+          <p className="text-right text-3xl font-medium">SetPato</p>
+          <p className="mt-5 text-right font-extralight">
+            Joined the team in 2022, working as a Front-end engineer. Focused on
+            building on Bitcoin and Stacks, always looking to develope and code.
+            I always make sure to be the dummest guy in the room to keep
+            learning and growing.
+          </p>
+          <p className="mt-5 text-right font-extralight">
+            Fun Fact: I played for the Men's National Ice Hockey Team.
+          </p>
+          <p className="mt-5 text-right font-bold">@setpato</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="h-[280px]">
+          <p className="text-right text-3xl font-medium">Who Are We</p>
+          <p className="mt-5 text-right font-extralight">
+            We’re a tiny but fiery team with more than a decade in software
+            engineering & product development. We recently spent the majority of
+            our time working within the Stacks community & slowly working our
+            way to building on Bitcoin. Through this journey, we realized the
+            significant gap in educational content & development tooling in the
+            most important chain of them all.
+          </p>
+          <p className="mt-5 text-right font-bold">
+            Hover over any of the portraits to learn more about a teammate.
+          </p>
+        </div>
+      );
+    }
+  };
   return (
-    <div className="ml-10 mr-10 mt-5 flex flex-col text-black md:ml-[260px]">
+    <div className="ml-10 mr-10 mt-10 flex flex-col text-black md:ml-[260px]">
       {/* About Bitscript */}
       <div className="flex flex-col items-center justify-between xl:flex-row">
         {/* Text */}
-        <div className="flex flex-col xl:mr-[200px]">
+        <div className="flex flex-col xl:w-full">
           <p onClick={() => handleClick()} className="text-3xl font-medium">
             About BitScript
           </p>
@@ -36,48 +107,34 @@ const About = () => {
           </p>
           <p className="mt-5 font-extralight">
             The first of these is a transaction{" "}
-            <Link
-              href={"/transactions"}
-              target="_blank"
-              className="font-bold underline"
-            >
+            <Link href={"/transactions"} target="_blank" className=" underline">
               deserializer
             </Link>{" "}
             that’s in MVP now. 
           </p>
         </div>
         <div className="mt-10 flex xl:mt-0">
-          <LogoWBg />
+          <img src="Logo-with-bg.png" alt="Logo" className="ml-10 w-[700px]" />
         </div>
       </div>
       {/* Who Are We */}
-      <div className="mt-20 flex flex-col items-center justify-between xl:flex-row">
+      <div className="mt-10 flex flex-col items-center justify-between xl:flex-row">
         <div className="hidden xl:flex">
           <PfpTeam />
         </div>
-        {/* Text */}
-        <div className="flex flex-col xl:ml-[600px]">
-          <p className="text-right text-3xl font-medium">Who Are We</p>
-          <p className="mt-5 text-right font-extralight">
-            We’re a tiny but fiery team with more than a decade in software
-            engineering & product development. We recently spent the majority of
-            our time working within the Stacks community & slowly working our
-            way to building on Bitcoin. Through this journey, we realized the
-            significant gap in educational content & development tooling in the
-            most important chain of them all.
-          </p>
-          {/* <p className="mt-5 text-right font-bold">
-            Hover over any of the portraits to learn more about a teammate.
-          </p> */}
+        {/* Text Team */}
+        <div className="flex flex-col xl:w-[500px]">
+          {" "}
+          {renderTeamMemberInfo()}
         </div>
         <div className="mt-10 flex xl:hidden">
           <PfpTeam />
         </div>
       </div>
       {/* Contact Or Feedback */}
-      <div className="mb-20 mt-20 flex flex-col items-center justify-between xl:flex-row">
+      <div className="mb-20 mt-10 flex flex-col items-center justify-between xl:flex-row">
         {/* Text */}
-        <div className="flex flex-col xl:mr-[200px]">
+        <div className="flex flex-col xl:w-[1000px]">
           <p className=" text-3xl font-medium">Contact Or Feedback?</p>
           <p className="mt-5  font-extralight">
             Our goal is to grow alongside the community of developers leveraging
@@ -105,7 +162,7 @@ const About = () => {
             community.
           </p>
         </div>
-        <div className="mt-10 flex xl:mt-0">
+        <div className="ml-10 mt-10 flex w-full lg:w-auto xl:mt-0">
           <FormAbout />
         </div>
       </div>
--- src/comp/About/Form.tsx
+++ src/comp/About/Form.tsx
@@ -1,17 +1,55 @@
+import React, { useState } from "react";
+
 const FormAbout = () => {
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+
+  // Regular expression for email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Function to handle email input changes
+  const handleEmailChange = (event: { target: { value: any } }) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+
+    // Check if the input matches the email regex
+    setIsValidEmail(emailRegex.test(newEmail));
+  };
+
   return (
-    <div className="flex flex-col">
+    <div className="-ml-5 flex w-full flex-col justify-center">
       <p className="font-extralight text-[#BBBBBB]">Email Message</p>
       <textarea
-        className="h-[182px] w-[440px] rounded-3xl border border-[#BBBBBB] p-4 font-bold outline-none md:w-[480px]"
-        placeholder="type your message here..."
+        className="h-[182px] rounded-3xl border border-[#BBBBBB] p-4 font-bold text-black outline-none md:w-[480px]"
+        placeholder="Type your message here..."
       />
       <p className="font-extralight text-[#BBBBBB]">Email Address</p>
-      <input
-        type="text"
-        className="h-[56px] w-[440px] rounded-3xl border border-[#BBBBBB] p-4 font-bold outline-none md:w-[480px]"
-        placeholder="youremail@example.com"
-      />
+      <div className="relative">
+        <input
+          type="text"
+          value={email}
+          onChange={handleEmailChange}
+          className={`h-[56px] w-full rounded-3xl border border-[#BBBBBB] p-4 font-bold text-black outline-none md:w-[480px]
+            `}
+          placeholder="youremail@example.com"
+        />
+
+        <svg
+          width="23"
+          height="22"
+          viewBox="0 0 23 22"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute right-3 top-1/2 -mt-3 rounded-full"
+        >
+          <path
+            d="M11.5 0.25C5.572 0.25 0.75 5.073 0.75 11C0.75 16.927 5.572 21.75 11.5 21.75C17.428 21.75 22.25 16.927 22.25 11C22.25 5.073 17.428 0.25 11.5 0.25ZM11.5 20.25C6.399 20.25 2.25 16.101 2.25 11C2.25 5.899 6.399 1.75 11.5 1.75C16.601 1.75 20.75 5.899 20.75 11C20.75 16.101 16.601 20.25 11.5 20.25ZM15.53 8.13599C15.823 8.42899 15.823 8.90402 15.53 9.19702L10.863 13.864C10.717 14.01 10.525 14.084 10.333 14.084C10.141 14.084 9.94901 14.011 9.80301 13.864L7.47 11.531C7.177 11.238 7.177 10.763 7.47 10.47C7.763 10.177 8.23801 10.177 8.53101 10.47L10.334 12.273L14.47 8.13702C14.763 7.84402 15.237 7.84399 15.53 8.13599Z"
+            className={`${
+              isValidEmail ? "fill-[#F79327]" : "fill-gray-500 opacity-50"
+            }`}
+          />
+        </svg>
+      </div>
     </div>
   );
 };
