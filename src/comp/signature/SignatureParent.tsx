16b38d95f67a1e66ab1d99d0f8ed6034c8a0573c
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-12-23 13:49:16 -0500

    chore: edcsa generate sig header

--- src/comp/signature/SignatureParent.tsx
+++ src/comp/signature/SignatureParent.tsx
@@ -1,10 +1,66 @@
+import { useState } from "react";
+import { EDCSA_STEPS } from "./const";
+
+import { classNames, useIsMobile, useWindowSize } from "@/utils";
+
+const stepsBasicStyling = `font-thin  text-[48px]`;
+
+type ECDSAHeader = {
+  currentStep: number;
+};
+const ECDSAGenerateHeader = ({ currentStep }: ECDSAHeader) => {
+  const isSelected = (step: number) => {
+    if (currentStep == 2 && step == 1) {
+      return "!font-bold text-[50px] text-dark-orange";
+    }
+
+    if (step === currentStep) {
+      return "!font-bold text-[50px] text-dark-orange";
+    }
+
+    if (step < currentStep) {
+      return "!font-semibold text-[50px] text-black";
+    }
+  };
+
+  return (
+    <div className="flex w-full flex-row">
+      <div className="flex  flex-col ">
+        <p className="text-[48px] font-semibold tracking-widest	 text-black">
+          ECDSA
+        </p>
+        <p className="text-[18px] font-light text-black">Generate Signature</p>
+      </div>
+      <p className="w-min text-[48px] font-semibold text-black">=</p>
+      <div className="ml-4 flex  flex-row gap-2">
+        <p className={classNames(stepsBasicStyling, "relative", isSelected(1))}>
+          K{" "}
+          <span className="absolute -right-4 -top-1.5 text-[23px] font-bold">
+            -1
+          </span>
+        </p>
+        <p className={classNames(stepsBasicStyling, "ml-2")}>{"("}</p>
+        <p className={classNames(stepsBasicStyling, "", isSelected(2))}>H(m)</p>
+        <p className={classNames(stepsBasicStyling, "")}>+</p>
+        <p className={classNames(stepsBasicStyling, "", isSelected(3))}>e</p>
+        <p className={classNames(stepsBasicStyling, "")}>*</p>
+        <p className={classNames(stepsBasicStyling, "", isSelected(4))}>r</p>
+        <p className={classNames(stepsBasicStyling, "")}>{")"}</p>
+      </div>
+    </div>
+  );
+};
+
 const SignatureParent = () => {
+  const [step, setStep] = useState(1);
+
   return (
     <div className="mx-10 mb-10 mt-10 md:ml-[260px] md:mr-5">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-4">
         <div className="flex flex-col">
           <p className="font-extralight text-[#687588]">Utility Tool</p>
         </div>
+        <ECDSAGenerateHeader currentStep={step} />
       </div>
     </div>
   );
