dc7ea6773d22e9c486607858b8a3a0af96e63f06
Bernardo Garciarivas <brgarciarivas@gmail.com>
2024-01-10 00:07:25 -0500

    chore: enable shwoing image

--- src/comp/Transactions/PopUpSections/PushedData.tsx
+++ src/comp/Transactions/PopUpSections/PushedData.tsx
@@ -6,6 +6,7 @@ import { knownScriptsAtom, txDataAtom } from "../TransactionsView";
 import { OP_CODES } from "@/utils/OPS";
 import { SCRIPTS_LIST } from "@/utils/SCRIPTS";
 import dynamic from "next/dynamic";
+import { ScriptData } from "@/corelibrary/scriptdata";
 const DynamicReactJson = dynamic(import("react-json-view"), { ssr: false });
 
 const PushedData = (props: TransactionItem) => {
@@ -177,6 +178,7 @@ const PushedData = (props: TransactionItem) => {
         }
       });
 
+      // get the last item in the array since it'll be the closest to our current index (which is our data)
       const mimeItem = mimeItems?.[mimeItems.length - 1];
       console.log("mimeItem", mimeItem);
 
@@ -195,6 +197,34 @@ const PushedData = (props: TransactionItem) => {
             </p>
           );
         } else if (mimeType === "image/svg+xml") {
+          const scriptdataItem = ScriptData.fromHex(props.item.value);
+          console.log("scriptdataItem", scriptdataItem);
+
+          const svg = scriptdataItem.dataString;
+          console.log("svg", svg);
+          // dangerouslySetInnerHTML={{ __html: svg }}
+
+          const urlPath = "https://ordinals.com/";
+
+          // find every instance of a url path and append urlPath to the beginning of it
+          const newSvg = props.item.value.replace(
+            /href="\//g,
+            `href="${urlPath}`
+          );
+
+          // remove the last character from the string
+          const _newSvg = newSvg.slice(0, -1);
+          console.log("_newSvg", _newSvg);
+          const blob = new Blob([_newSvg], { type: "text/html" });
+          const url = URL.createObjectURL(blob);
+
+          return (
+            <iframe
+              src={url}
+              sandbox="allow-scripts"
+              style={{ width: "300px", height: "300px", border: "none" }}
+            ></iframe>
+          );
           // return the svg image
           return null;
         }
@@ -225,3 +255,26 @@ const PushedData = (props: TransactionItem) => {
 };
 
 export default PushedData;
+
+/*
+"<svg id="ThePepes" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+ <svg>
+  <image xlink:href="/content/b7cdcb669bb4cd86828359bfe076c293040220a514f07f18ac855b634ce162bbi0" width="100%" height="100%"></image>
+ </svg>
+ <svg>
+  <image xlink:href="/content/221f3953d4ad0a6f19f02cb399dc658fc91ae635746b4e32039c5d357db5e3a0i0" width="100%" height="100%"></image>
+ </svg>
+ <svg>
+  <image xlink:href="/content/9ee104f7e03c73ecf60b95ae4d35cbd14da9a269f6792ca65678daca4f8e6e9Mö7i0" width="100%" height="100%"></image>
+ </svg>
+ <svg>
+  <image xlink:href="/content/c2690143a60efe68e9a42c125778b5e4dc0c25d3c04b7c2f8c3d3742d151474bi0" width="100%" height="100%"></image>
+ </svg>
+ <svg>
+  <image xlink:href="/content/b916c9c38e38e7dbee1fbb517e0f1a525e661d1557fa066e3813ff9c503f00f4i0" width="100%" height="100%"></image>
+ </svg>
+ <svg>
+  <image xlink:href="/content/58f3e9c7ca2a2164a3d492a85ead8810f62520bd5de8b05d96efb2671d103760i0" width="100%" height="100%"></image>
+ </svg>
+</svg>h"
+*/
