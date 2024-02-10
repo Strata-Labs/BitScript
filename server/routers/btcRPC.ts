58174495d9922114969897ad3165af218f7796b9
Bernardo Garciarivas <brgarciarivas@gmail.com>
2024-02-10 12:53:33 -0500

    chore: set up pages for rpcs

--- server/routers/btcRPC.ts
+++ server/routers/btcRPC.ts
@@ -14,6 +14,7 @@ export const fetchBTCRPC = procedure
       var myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
 
+      console.log("opts", opts.input.params);
       var raw = JSON.stringify({
         method: opts.input.method,
         params: opts.input.params,
@@ -26,19 +27,25 @@ export const fetchBTCRPC = procedure
       };
 
       const btcRpcRes_ = await fetch(
-        "https://docs-demo.btc.quiknode.pro/",
+        "https://withered-rough-lake.btc.quiknode.pro/f46b3a795512b0cf36f9607866beea5bd10ce940/",
         requestOptions
       );
 
+      console.log("btcRpcRes_", btcRpcRes_);
       const btcRpcRes: any = await btcRpcRes_.json();
 
       console.log("btcRpcRes", btcRpcRes);
-      if (btcRpcRes.result) {
+      if (btcRpcRes.result !== null) {
         return btcRpcRes.result;
       } else {
-        throw new Error(btcRpcRes.error.message);
+        if (btcRpcRes.error !== null) {
+          throw new Error(btcRpcRes.error.message);
+        } else {
+          throw new Error("Resource could not be found");
+        }
       }
     } catch (err: any) {
+      console.log("err", err);
       throw new Error(err);
     }
   });
---
+++ src/comp/RPC/RpcListView.tsx
@@ -0,0 +1,5 @@
+const RpcListView = () => {
+  return "hi";
+};
+
+export default RpcListView;
