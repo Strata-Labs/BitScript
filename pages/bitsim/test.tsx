9e7ef68e6d8e5a6ce667f2c7bdd552d81a87669a
Patotking12 <pato2k5@gmail.com>
2024-03-13 12:46:51 -0600

    Issue 636

--- pages/bitsim/test.tsx
+++ pages/bitsim/test.tsx
@@ -3,7 +3,7 @@ import SearchView from "@/comp/SearchView/SearchView";
 import { activeSearchView, menuOpen } from "@/comp/atom";
 import { useAtom } from "jotai";
 
-export default function BitSim() {
+export default function BitSimTests() {
   const [showSearchView] = useAtom(activeSearchView);
   const [isMenuOpen, setMenuOpen] = useAtom(menuOpen);
 
