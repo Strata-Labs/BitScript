10cb8ae8ff1e43adb0e4d2df971a989b127c3a54
Bernardo Garciarivas <brgarciarivas@gmail.com>
2024-03-30 03:32:02 -0400

    chore: basics for node flows

--- pages/taprootPreview.tsx
+++ pages/taprootPreview.tsx
@@ -4,6 +4,7 @@ import { useAtom } from "jotai";
 import HashedKey from "@/comp/Data Fields/HashedKey";
 import FeaturePreview from "@/comp/FeaturePreview";
 
+import MerkleTreeNodes from "@/comp/TaprootTools/MerkleTreeNodes";
 export default function page() {
   const [showSearchView] = useAtom(activeSearchView);
   const [isMenuOpen, setMenuOpen] = useAtom(menuOpen);
@@ -12,5 +13,5 @@ export default function page() {
     return null;
   }
 
-  return <div>{showSearchView ? <SearchView /> : <FeaturePreview />}</div>;
+  return <div>{showSearchView ? <SearchView /> : <MerkleTreeNodes />}</div>;
 }
