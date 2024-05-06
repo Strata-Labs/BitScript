33473ae7df4efc585227d87f862f5bb52c39965c
Chigala <92148630+Chigala@users.noreply.github.com>
2024-05-06 18:01:29 +0100

    chore: added custom head elements for lessons, rpcs and ops

--- src/comp/RPC/rpcMainView.tsx
+++ src/comp/RPC/rpcMainView.tsx
@@ -2,6 +2,7 @@ import { RPCFunctionParams } from "@/const/RPC";
 import RpcLeftColumn from "./rpcLeftColumn";
 import RpcRightColumn from "./rpcRightColumn";
 import RpcTopRight from "./rpcTopRight";
+import CustomHead from "@/comp/CustomHead";
 
 type RpcMainViewProps = {
   method: RPCFunctionParams;
@@ -9,6 +10,9 @@ type RpcMainViewProps = {
 const RpcMainView = ({ method }: RpcMainViewProps) => {
   return (
     <div className="h-screen md:ml-[240px]">
+      <CustomHead
+        meta={{ title: method.method, description: method.description }}
+      />
       <div className="flex h-full w-full flex-col md:flex-row">
         <RpcLeftColumn method={method} />
         <div className="mt-5 flex h-full w-full flex-col md:ml-5 md:mt-0">
