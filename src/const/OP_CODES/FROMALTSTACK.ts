473f3c8858ac8dd2a50e3ca215bbf1d84d6aee42
Chigala <92148630+Chigala@users.noreply.github.com>
2024-05-22 14:35:06 +0100

    feat: added OP_fromaltstack animation

---
+++ src/const/OP_CODES/FROMALTSTACK.ts
@@ -0,0 +1,294 @@
+import {
+  COLUMN_TYPE,
+  EXECUTION_STEPS,
+  LIB_DATA_TYPE,
+  MOVE_TYPE,
+  SCRIPT_DATA_STYLE_TYPE,
+} from "@/OPS_ANIMATION_LIB";
+import { OP_CODE_PAGE_PROPS } from "@/comp/opCodes/OpCodeView";
+import TileImage from "@/../public/images/DUP_TILE.svg";
+
+export const OP_FROMALTSTACK_STEPS: EXECUTION_STEPS[] = [
+  {
+    // First Video
+    containers: [0],
+    mainStack: [],
+    resultStack: [],
+    actions: [
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.MAIN_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+          stackIndex: 0,
+          dataHex: "01000000",
+          dataNumber: "Altitem1",
+          className: "COLUMN-0-0",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        },
+      },
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.MAIN_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+          stackIndex: 0,
+          dataHex: "01000000",
+          dataNumber: "Altitem2",
+          className: "COLUMN-0-1",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        },
+      },
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.MAIN_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+          stackIndex: 0,
+          dataHex: "01000000",
+          dataNumber: "Altitem3",
+          className: "COLUMN-0-2",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        },
+      },
+    ],
+  },
+  // Second Video
+  
+  {
+    containers: [1],
+    mainStack: [
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        dataHex: "01000000",
+        dataNumber: "Altitem1",
+        stackIndex: 0,
+        className: "COLUMN-1-0",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        dataHex: "01000000",
+        dataNumber: "Altitem2",
+        stackIndex: 0,
+        className: "COLUMN-1-1",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        dataHex: "01000000",
+        dataNumber: "Altitem3",
+        stackIndex: 0,
+        className: "COLUMN-1-2",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+    ],
+    resultStack: [],
+    actions: [
+      {
+        moveType: MOVE_TYPE.ADD,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 0,
+        data: {
+          name: "FROMALTSTACK",
+          number: 118,
+          hex: "0x76",
+          stackIndex: 0,
+          description: "Remvoes the top item from the alternate stack and pushes it to the main stack",
+          className: "COLUMN-2-0",
+          libDataType: LIB_DATA_TYPE.OP_CODE,
+        },
+      },
+      {
+        moveType: MOVE_TYPE.MOVE_POP_ARROW,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 1,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          dataHex: "01000000",
+          dataNumber: "item3",
+          stackIndex: 1,
+          className: "COLUMN-1-1",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+          styleType: SCRIPT_DATA_STYLE_TYPE.DUPLICATE,
+        },
+      },
+    ],
+  },
+  // Fourth Video
+  {
+    containers: [2],
+    mainStack: [
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        stackIndex: 0,
+        dataHex: "01000000",
+        dataNumber: "Altitem3",
+        className: "COLUMN-1-0",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+    ],
+    resultStack: [
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        stackIndex: 0,
+        dataHex: "01000000",
+        dataNumber: "item1",
+        className: "COLUMN-2-0",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        stackIndex: 0,
+        dataHex: "01000000",
+        dataNumber: "item2",
+        className: "COLUMN-2-1",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+      {
+        dataBinary: {},
+        dataBytes: {
+          "0": 1,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+        },
+        stackIndex: 0,
+        dataHex: "01000000",
+        dataNumber: "item3",
+        className: "COLUMN-2-2",
+        libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+        styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+      },
+    ],
+    actions: [
+      {
+        moveType: MOVE_TYPE.MOVE_POP_ARROW,
+        to: COLUMN_TYPE.RESULT_STACK,
+        stackIndex: 0,
+        data: {
+          dataBinary: {},
+          dataBytes: {
+            "0": 1,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+          },
+          stackIndex: 1,
+          dataHex: "01000000",
+          dataNumber: 1,
+          className: "COLUMN-1-0",
+          libDataType: LIB_DATA_TYPE.SCRIPT_DATA,
+          styleType: SCRIPT_DATA_STYLE_TYPE.BASIC,
+        },
+      },
+    ],
+  },
+
+
+];
+
+export const OP_FROMALTSTACK: OP_CODE_PAGE_PROPS = {
+  name: "OP_FROMALTSTACK",
+  opCode: "108",
+  hex: "0x6c",
+  category: "Stack",
+  shortDescription:
+    "Moves the top item from the alternate stack to the main stack.",
+  longDescription:
+    "The OP_FROMALTSTACK operation transfers the top item from the alternate stack to the main stack. This opcode is useful in scripts where temporary storage of items is necessary. By using the alternate stack, scripts can manage and retrieve data without disrupting the main stack's flow of operations.",
+  inputNum: "1",
+  inputType: "Any",
+  returnNum: "1",
+  returnType: "Any",
+  seenIn: "N/A",
+  status: "Committed",
+  linkPath: "/OPS/OP_FROMALTSTACK",
+  tileImage: TileImage,
+  type: "Pop & Push",
+  generalType: "OpCode",
+  longName: "",
+  visualProps: {
+    stackSteps: OP_FROMALTSTACK_STEPS,
+    title: "OP_Code Walkthrough",
+    failureSteps: OP_FROMALTSTACK_STEPS,
+    description:
+      "Moves the top item from the alternate stack to the main stack.",
+    steps: [
+      "Pop top item from the alternate stack",
+      "Push item to the main stack",
+    ],
+  },
+};
