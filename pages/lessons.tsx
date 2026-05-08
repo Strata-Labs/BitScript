886519bdf9917a80e3e9270ac63b7193da25988b
Chigala <chigala11@gmail.com>
2026-05-08 07:22:26 +0100

    load nav namespace on lessons listing page

--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -2,6 +2,8 @@ import SearchView from "@/comp/SearchView/SearchView";
 import { activeSearchView, menuOpen } from "@/comp/atom";
 import { useAtom } from "jotai";
 import Tutorials from "@/comp/Tutorials/Tutorials";
+import type { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function TEMP() {
   const [showSearchView] = useAtom(activeSearchView);
@@ -13,3 +15,13 @@ export default function TEMP() {
 
   return <div>{showSearchView ? <SearchView /> : <Tutorials />}</div>;
 }
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? "en", [
+      "common",
+      "nav",
+      "lessons",
+    ])),
+  },
+});
