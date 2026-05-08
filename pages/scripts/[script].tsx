7af7ce4ecaf56853c3b94b15d1c9c31d231380c5
Chigala <chigala11@gmail.com>
2026-05-08 08:37:57 +0100

    localize script detail page via registry

--- pages/scripts/[script].tsx
+++ pages/scripts/[script].tsx
@@ -5,6 +5,7 @@ import { SCRIPTS_LIST } from "@/utils/SCRIPTS";
 import { activeSearchView } from "@/comp/atom";
 import { useAtom } from "jotai";
 import SearchView from "@/comp/SearchView/SearchView";
+import { getLocalizedScript } from "@/const/SCRIPTS/translations";
 import type { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
@@ -21,7 +22,8 @@ export default function scriptPagesHandler() {
     // find the script based on the query
     const SCR = SCRIPTS_LIST.find((_script) => _script.shortHand === script);
     if (SCR) {
-      return <ScriptView {...SCR} />;
+      const localized = getLocalizedScript(SCR, routerScripts.locale);
+      return <ScriptView {...localized} />;
     } else {
       return <ScriptsPage />;
     }
