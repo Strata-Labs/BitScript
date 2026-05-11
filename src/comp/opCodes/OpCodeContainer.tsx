8b37dc9a767e5be2617d75301a2db256860f1c85
Chigala <chigala11@gmail.com>
2026-05-11 16:45:58 +0100

    Make OpCodeContainer responsive and wrap badges

--- src/comp/opCodes/OpCodeContainer.tsx
+++ src/comp/opCodes/OpCodeContainer.tsx
@@ -31,7 +31,7 @@ const OpCodeContainer: React.FC<OpCodeContainerProps> = ({
       <div className="flex w-screen justify-center md:w-full">
         {/* General white background container */}
         <div
-          className="group mx-[80px] flex h-[200px] w-full flex-col justify-between rounded-xl bg-white from-[#100F20] to-[#321B3A] p-4 px-5 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:bg-gradient-to-b md:h-[200px] md:w-[274px] md:rounded-xl"
+          className="group mx-[80px] flex min-h-[200px] w-full flex-col justify-between rounded-xl bg-white from-[#100F20] to-[#321B3A] p-4 px-5 transition-all duration-500 ease-in-out hover:-translate-y-1 hover:bg-gradient-to-b md:min-h-[200px] md:w-[274px] md:rounded-xl"
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
@@ -56,16 +56,16 @@ const OpCodeContainer: React.FC<OpCodeContainerProps> = ({
             </p>
           </div>
           {/* Bottom part of the container */}
-          <div className="mx-4 flex items-center justify-between space-x-1 md:mx-6">
+          <div className="mt-4 flex flex-wrap items-center justify-center gap-2">
             {/* Left Rectangle circle with grayish background */}
-            <div className="flex h-[27px] w-[80px] items-center justify-center rounded-full bg-[#F4F4F4] transition-all duration-500 ease-in-out group-hover:bg-[#3E314A] ">
-              <p className="text-[12px] font-extralight text-[#6C5E70] transition-all duration-500 ease-in-out group-hover:text-[#FFFFFF]">
+            <div className="flex h-[27px] min-w-[80px] items-center justify-center rounded-full bg-[#F4F4F4] px-4 transition-all duration-500 ease-in-out group-hover:bg-[#3E314A] ">
+              <p className="whitespace-nowrap text-[12px] font-extralight text-[#6C5E70] transition-all duration-500 ease-in-out group-hover:text-[#FFFFFF]">
                 {category}
               </p>
             </div>
             {/* Right Rectangle circle with grayish background */}
-            <div className="flex h-[27px] w-[80px] items-center justify-center rounded-full bg-[#F4F4F4] transition-all duration-500 ease-in-out group-hover:bg-[#3E314A] ">
-              <p className="text-[12px] font-extralight text-[#6C5E70] transition-all duration-500 ease-in-out group-hover:text-[#FFFFFF]">
+            <div className="flex h-[27px] min-w-[80px] items-center justify-center rounded-full bg-[#F4F4F4] px-4 transition-all duration-500 ease-in-out group-hover:bg-[#3E314A] ">
+              <p className="whitespace-nowrap text-[12px] font-extralight text-[#6C5E70] transition-all duration-500 ease-in-out group-hover:text-[#FFFFFF]">
                 {type}
               </p>
             </div>
